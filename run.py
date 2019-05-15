9bb12499bb3a2155712c8efe41fdcaea2bb55cbc
rakeshgm <rgowdege@redhat.com>
2019-05-15 20:53:30 +0530

    added rhcs4 baseurl
    Signed-off-by: rakeshgm <rgowdege@redhat.com>

--- run.py
+++ run.py
@@ -280,6 +280,13 @@ def run(args):
         if ubuntu_repo is None:
             ubuntu_repo = \
                 'http://download.eng.bos.redhat.com/rcm-guest/ceph-drops/3.2/latest-RHCEPH-3.2-Ubuntu/'
+    elif rhbuild.startswith('4'):
+        if base_url is None:
+            # default to latest RHCeph build 4.0
+            base_url = 'http://download.eng.bos.redhat.com/rhel-8/composes/auto/ceph-4.0-rhel-8/latest-RHCEPH-4-RHEL-8/'
+        if ubuntu_repo is None:
+            ubuntu_repo = \
+                'http://download.eng.bos.redhat.com/rcm-guest/ceph-drops/3.2/latest-RHCEPH-3.2-Ubuntu/'
     if os.environ.get('TOOL') is not None:
         ci_message = json.loads(os.environ['CI_MESSAGE'])
         compose_id = ci_message['compose_id']
