454049600dc08a1bd40c9ce36b23d959198ccd4a
ameenasuhani <ameenasuhani@gmail.com>
2020-08-13 05:54:55 +0000

    docker to podman migration with ansible
    Signed-off-by: ameenasuhani <ameenasuhani@gmail.com>

---
+++ tests/ceph_ansible/docker_to_podman.py
@@ -0,0 +1,39 @@
+'''Migrates from docker to podman'''
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def run(**kw):
+
+    log.info("Running exec test")
+    ceph_nodes = kw.get('ceph_nodes')
+    config = kw.get('config')
+    build = config.get('rhbuild')
+    installer_node = None
+    ansible_dir = '/usr/share/ceph-ansible'
+    playbook = 'docker-to-podman.yml'
+    for cnode in ceph_nodes:
+        if cnode.role == 'installer':
+            installer_node = cnode
+    if not build.startswith('4'):
+        installer_node.exec_command(sudo=True,
+                                    cmd='cd {ansible_dir}; cp {ansible_dir}/infrastructure-playbooks/{playbook} .'
+                                    .format(ansible_dir=ansible_dir, playbook=playbook))
+        out, err = installer_node.exec_command(cmd='cd {ansible_dir};ansible-playbook -vvvv {playbook}'
+                                               ' -e ireallymeanit=yes -i hosts'
+                                               .format(ansible_dir=ansible_dir, playbook=playbook),
+                                               long_running=True)
+
+    else:
+        out, err = installer_node.exec_command(cmd='cd {ansible_dir};ansible-playbook -vvvv'
+                                               ' infrastructure-playbooks/{playbook} -e ireallymeanit=yes -i hosts'
+                                               .format(ansible_dir=ansible_dir, playbook=playbook),
+                                               long_running=True)
+
+    if err == 0:
+        log.info("ansible-playbook docker-podman successful")
+        return 0
+
+    log.info("ansible-playbook docker-podman failed")
+    return 1
