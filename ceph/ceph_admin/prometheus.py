a8cbf4c75ff4e01660ae40aa9bfd6d88e6c075b2
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-02-15 10:12:52 +0530

    Moving prometheus to new design style
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- ceph/ceph_admin/prometheus.py
+++ ceph/ceph_admin/prometheus.py
@@ -1,14 +1,36 @@
-"""
-Module to deploy Prometheus service and individual daemon(s).
+"""Module to deploy Prometheus service and individual daemon(s)."""
+from typing import Dict
 
-this module deploy Prometheus service and daemon(s) along with
-handling other prerequisites needed for deployment.
-
-"""
-from ceph.ceph_admin.apply import ApplyMixin
-
+from .apply import ApplyMixin
 from .orch import Orch
 
 
 class Prometheus(ApplyMixin, Orch):
     SERVICE_NAME = "prometheus"
+
+    def apply(self, config: Dict) -> None:
+        """
+        Deploy the Prometheus service using the provided configuration.
+
+        Args:
+            config: Key/value pairs provided by the test case to create the service.
+
+        Example
+            config:
+                command: apply
+                service: prometheus
+                base_cmd_args:          # arguments to ceph orch
+                    concise: true
+                    verbose: true
+                    input_file: <name of spec>
+                args:
+                    placement:
+                        label: iscsi    # either label or node.
+                        nodes:
+                            - node1
+                        limit: 3    # no of daemons
+                        sep: " "    # separator to be used for placements
+                    dry-run: true
+                    unmanaged: true
+        """
+        super().apply(config=config)
