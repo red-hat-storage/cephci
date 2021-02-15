7eae6a305bbccb1b701322f3241a520c90e9b838
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-02-15 09:53:29 +0530

    support alertmanager
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- ceph/ceph_admin/alert_manager.py
+++ ceph/ceph_admin/alert_manager.py
@@ -1,4 +1,6 @@
 """Deploy the alert manager service in the cluster via cephadm CLI."""
+from typing import Dict
+
 from .apply import ApplyMixin
 from .orch import Orch
 
@@ -7,3 +9,30 @@ class AlertManager(ApplyMixin, Orch):
     """Manage the alert-manager service."""
 
     SERVICE_NAME = "alertmanager"
+
+    def apply(self, config: Dict) -> None:
+        """
+        Deploy the MDS service using the provided configuration.
+
+        Args:
+            config: Key/value pairs provided by the test case to create the service.
+
+        Example
+            config:
+                command: apply
+                service: alertmanager
+                base_cmd_args:          # arguments to ceph orch
+                    concise: true
+                    verbose: true
+                    input_file: <name of spec>
+                args:
+                    placement:
+                        label: iscsi    # either label or node.
+                        nodes:
+                            - node1
+                        limit: 3    # no of daemons
+                        sep: " "    # separator to be used for placements
+                    dry-run: true
+                    unmanaged: true
+        """
+        super().apply(config=config)
