9898413cd7619a93c7b598768ef8ae2b091e4ed5
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-02-15 09:56:02 +0530

    support mgr
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- ceph/ceph_admin/mgr.py
+++ ceph/ceph_admin/mgr.py
@@ -1,4 +1,5 @@
 """Manage the Manager service via Ceph's cephadm CLI."""
+from typing import Dict
 from .apply import ApplyMixin
 from .orch import Orch
 
@@ -7,3 +8,32 @@ class Mgr(ApplyMixin, Orch):
     """Manager interface for ceph orch <action> mgr"""
 
     SERVICE_NAME = "mgr"
+
+    def apply(self, config: Dict) -> None:
+        """
+        Deploy the MDS service using the provided configuration.
+
+        Args:
+            config: Key/value pairs provided by the test case to create the service.
+
+        Example
+            config:
+                command: apply
+                service: mgr
+                base_cmd_args:          # arguments to ceph orch
+                    concise: true
+                    verbose: true
+                    input_file: <name of spec>
+                pos_args:
+                    - india             # name of the filesystem
+                args:
+                    placement:
+                        label: iscsi    # either label or node.
+                        nodes:
+                            - node1
+                        limit: 3    # no of daemons
+                        sep: " "    # separator to be used for placements
+                    dry-run: true
+                    unmanaged: true
+        """
+        super().apply(config=config)
