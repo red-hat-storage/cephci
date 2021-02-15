b1e31b2a267f4f41f75661dadfc1b0c1f3598726
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-02-15 08:50:02 +0530

    Move iSCSI to new design
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- ceph/ceph_admin/iscsi.py
+++ ceph/ceph_admin/iscsi.py
@@ -1,5 +1,5 @@
 """Module to deploy and manage Ceph's iSCSI service."""
-from typing import Dict, Optional
+from typing import Dict
 
 from .apply import ApplyMixin
 from .orch import Orch
@@ -10,47 +10,44 @@ class ISCSI(ApplyMixin, Orch):
 
     SERVICE_NAME = "iscsi"
 
-    def apply(
-            self,
-            prefix_args: Optional[Dict] = None,
-            args: Optional[Dict] = None
-    ) -> None:
+    def apply(self, config: Dict) -> None:
         """
         Deploy ISCSI client daemon using the provided arguments.
 
-        Fixme: This method does not consider the prefix args to the base command.
-
         Args:
-            prefix_args:    Key/value pairs to be passed to the command.
-            args:           Key/value pairs to be passed as arguments to the command.
+            config: Key/value pairs provided from the test scenario
 
-        config:
-            command: apply
-            service: iscsi
-            prefix_args:
-                pool_name: india
-                pool_pg_num: 3
-                pool_pgp_num: 3
-                api_user: user
-                api_password: password
-                trusted_ip_list: list of ips
-            args:
-                label: iscsi    # either label or node.
-                nodes:
-                    - node1
-                limit: 3    # no of daemons
-                sep: " "    # separator to be used for placements
+        Example:
+            config:
+                command: apply
+                service: iscsi
+                base_cmd_args:          # arguments to ceph orch
+                    concise: true
+                    verbose: true
+                    input_file: <name of spec>
+                pos_args:
+                    - india             # name of the pool
+                    - api_user          # name of the API user
+                    - api_pass          # password of the api_user.
+                    - trusted_ip_list   # space separate list of IPs
+                args:
+                    placement:
+                        label: iscsi    # either label or node.
+                        nodes:
+                            - node1
+                        limit: 3    # no of daemons
+                        sep: " "    # separator to be used for placements
+                    dry-run: true
+                    unmanaged: true
+                temp_args:              # In place till OSD object is implemented.
+                    pool_pg_num: <count>
+                    pool_pgp_num: <count>
         """
-        pool = prefix_args.get("pool_name", "iscsi")
-        pg_num = prefix_args.get("pool_pg_num", 3)
-        pgp_num = prefix_args.get("pool_pgp_num", 3)
-        user = prefix_args.get("api_user", "user")
-        password = prefix_args.get("api_password", "password")
-        trusted_ip_list = prefix_args.get("trusted_ip_list", [])
-
-        prefix_list = list(
-            [pool, user, password, repr(" ".join(trusted_ip_list))]
-        )
+        # Temp workaround that needs to be removed
+        pool = config["ps_args"][0]
+        temp_args = config.pop("temp_args", {})
+        pg_num = temp_args.get("pool_pg_num", 3)
+        pgp_num = temp_args.get("pool_pgp_num", 3)
 
         # Execute pre-requisites
         self.shell(
@@ -69,4 +66,4 @@ class ISCSI(ApplyMixin, Orch):
         # Associate pool to RBD application
         self.shell(args=["ceph", "osd", "pool", "application", "enable", pool, "rbd"])
 
-        super().apply(prefix_args=prefix_list, args=args)
+        super().apply(config=config)
