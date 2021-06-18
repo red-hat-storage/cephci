16c0b0a41fb914b6aa206d7e1e09cbe3de616975
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-06-18 10:56:07 +0530

    Adding RGW Tier-2 jenkins file
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

---
+++ pipeline/5/Jenkinsfile-tier-2-object.groovy
@@ -0,0 +1,61 @@
+/*
+    Pipeline script for executing Tier 1 object test suites for RH Ceph 5.0.
+*/
+// Global variables section
+
+def nodeName = "centos-7"
+def cephVersion = "pacific"
+def sharedLib
+def testResults = [:]
+
+// Pipeline script entry point
+
+node(nodeName) {
+
+    timeout(unit: "MINUTES", time: 30) {
+        stage('Configure node') {
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: '*/master']],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [[
+                    $class: 'SubmoduleOption',
+                    disableSubmodules: false,
+                    parentCredentials: false,
+                    recursiveSubmodules: true,
+                    reference: '',
+                    trackingSubmodules: false
+                ]],
+                submoduleCfg: [],
+                userRemoteConfigs: [[url: 'https://github.com/red-hat-storage/cephci.git']]
+            ])
+            script {
+                sharedLib = load("${env.WORKSPACE}/pipeline/vars/common.groovy")
+                sharedLib.prepareNode()
+            }
+        }
+    }
+
+    timeout(unit: "HOURS", time: 4) {
+        stage('Regression') {
+            script {
+                withEnv([
+                    "sutVMConf=conf/inventory/rhel-8.4-server-x86_64-medlarge.yaml",
+                    "sutConf=conf/${cephVersion}/rgw/tier_2_rgw_regression.yaml",
+                    "testSuite=suites/${cephVersion}/rgw/tier_2_rgw_regression.yaml",
+                    "addnArgs=--post-results --log-level DEBUG"
+                ]) {
+                    rc = sharedLib.runTestSuite()
+                    testResults['Regression'] = rc
+                }
+            }
+        }
+    }
+
+    stage('Publish Results') {
+        script {
+            sharedLib.sendEMail("Object-Tier-2", testResults)
+        }
+    }
+
+}
