4aead30db9be265bf44ea18fa2192494e8988b5c
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2021-07-21 12:43:02 +0530

    Adding support for retrieving the user email address
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- cleanup_env.py
+++ cleanup_env.py
@@ -1,228 +1,224 @@
-""" Utility to cleanup instance/node along with its volumes"""
+"""
+    Utility to cleanup instances in RHOS-D environment that have crossed the maximum
+    allowed duration. There are multiple projects/tenants under CephQE purview and each
+    of them have a different configuration.
 
+    For example, ceph-jenkins has the least allowable time as the intent is to enable
+    the pipeline is executed under a stable environment. The durations for each project
+    are
+        ceph-jenkins        3 days
+        ceph-ci             2 weeks
+        ceph-core           2 weeks
+"""
 import smtplib
 import sys
 from datetime import datetime, timezone
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from typing import Dict, Optional
 
 import yaml
 from docopt import docopt
+from libcloud.common.openstack_identity import OpenStackIdentity_3_0_Connection
 from libcloud.common.types import LibcloudError
 from libcloud.compute.providers import get_driver
 from libcloud.compute.types import Provider
 
 from ceph.parallel import parallel
+from mita.v2 import CephVMNodeV2, Node
 from utility.retry import retry
 
 doc = """
-Utility to cleanup instance/node.
+Utility to cleanup instances in a RHOS cloud.
 
- Usage:
-  cleanup_env.py --osp-cred FILE
+    Usage:
+        cleanup_env.py --osp-cred <cred-file>
+        cleanup_env.py (-h | --help)
 
-Options:
-  --osp-cred <file>                 openstack credentials as separate file [default: osp/osp-cred-ci-2.yaml]
+    Options:
+        -h --help           Shows the command usage
+        --osp-cred <file>   API Credential file to access RHOS cloud.
 """
+OSP_IDENTITY: Optional[OpenStackIdentity_3_0_Connection] = None
 
 
-def get_instances_from_openstack_driver(yaml, tenant):
+def add_key_to_ref(
+    ref: Dict, category: str, email: str, vm_name: str, tenant: str
+) -> None:
     """
-    Create an openstack handle from config to manage RHOS-D resources
-    :param:
-       - yaml: openstack details as yaml file
-       - tenant: project name
-    :return: driver, openstack handle to retrieve driver info
+    Adds the provide information to ref
+
+    Arguments:
+        ref         Data structure to which the values need to be added
+        email       Primary identifier of the data structure
+        category    Whether the VM has been marked or deleted or has an error
+        vm_name     The name of instance
+        tenant      The project to which the instance belongs
+
+    Returns:
+        None but the ref is update with the provided values
     """
-    openstack = get_driver(Provider.OPENSTACK)
-    glbs = yaml.get("globals")
-    os_cred = glbs.get("openstack-credentials")
-    username = os_cred["username"]
-    password = os_cred["password"]
-    auth_url = os_cred["auth-url"]
-    auth_version = os_cred["auth-version"]
-    service_region = os_cred["service-region"]
-    domain_name = os_cred["domain"]
-    tenant_domain_id = os_cred["tenant-domain-id"]
-    driver = openstack(
-        username,
-        password,
-        ex_force_auth_url=auth_url,
-        ex_force_auth_version=auth_version,
-        ex_tenant_name=tenant,
-        ex_force_service_region=service_region,
-        ex_domain_name=domain_name,
-        ex_tenant_domain_id=tenant_domain_id,
+
+    if ref.get(email, {}).get(category, {}).get(tenant, []):
+        ref[email][category][tenant].append(vm_name)
+    else:
+        ref[email][category][tenant] = [vm_name]
+
+
+def cleanup(osp_cred: Dict, node: Node, results: Dict, tenant: str) -> None:
+    """
+    Removes VMs whose subscription has expired.
+
+    The instances are not removed if they have do-not-delete or is locked. They are
+    deleted if they have crossed the specified duration.
+
+    Arguments:
+         osp_cred       Credential information for accessing the cloud
+         node           The NodeDriver instance of the VM
+         results        Captures the results of the operation
+         tenant         The project to which the node belongs
+
+    Returns:
+        None
+    """
+    global OSP_IDENTITY
+
+    if "do-not-delete" in node.name.lower():
+        return
+
+    user_ = OSP_IDENTITY.get_user(node.extra["userId"])
+    if node.state.lower() != "error":
+        node_age = datetime.now(timezone.utc) - node.created_at
+        max_age = 3 if tenant == "ceph-jenkins" else 14
+
+        if node_age.days < (max_age - 1):
+            if user_.name == "psi-ceph-jenkins":
+                return
+
+            add_key_to_ref(results, "warn", user_.email, node.name, tenant)
+            return
+
+        if node_age.days < max_age:
+            return
+
+    ceph_node = CephVMNodeV2(
+        username=osp_cred["username"],
+        password=osp_cred["password"],
+        auth_url=osp_cred["auth-url"],
+        auth_version=osp_cred["auth-version"],
+        tenant_name=tenant,
+        tenant_domain_id=osp_cred["tenant-domain-id"],
+        service_region=osp_cred["service-region"],
+        domain_name=osp_cred["domain"],
+        node_name=node.name,
     )
-    return driver
 
-
-def instance_cleanup(instance, results, volumes, driver, instances_dict):
-    """
-    Remove specified instance and its volumes if any from RHOS-D
-    :param:
-       - instance: instance object need to be removed
-       - results: list: for storing instance which are failed to remove/delete
-       - volumes: volume information
-       - driver: openstack handler
-       - instances_dict: dict: for storing instance name as key and
-            date of its creation as value in case of success in its removal
-    """
     try:
-        for v in volumes:
-            if instance.name in v.name:
-                print("Removing volume:", v.name)
-                volobj = driver.ex_get_volume(v.id)
-                driver.detach_volume(volobj)
-                driver.destroy_volume(volobj)
-        print("Removing instance:", instance.name)
-        instance.destroy()
-        instances_dict[instance.name] = instance.extra["created"].split("T")[0]
-    except Exception:
-        results.append(instance.name)
+        ceph_node.delete()
+        if user_.name == "psi-ceph-jenkins":
+            # PSI Ceph Jenkins is a service account. Hence, ignoring email
+            return
+
+        add_key_to_ref(results, "deleted", user_.email, node.name, tenant)
+    except BaseException:  # noqa
+        add_key_to_ref(results, "marked", user_.email, node.name, tenant)
 
 
-def cleanup_instances_with_error(osp_cred):
+def send_email(payload: Dict) -> None:
     """
-    Removes instance that are in error state and its volumes if any
-    and Send email about the removed instances
-    :param:
-       - osp_cred: openstack details
-    :return: error_results:contains list of instance name in case of failure else empty list
-    """
-    error_results = []
-    dict_instance = dict()
-    projects = ["ceph-ci", "ceph-core", "ceph-jenkins"]
-    with parallel() as p:
-        for tenant in projects:
-            print("Removing instance that are in Error state from Project:", tenant)
-            driver = get_instances_from_openstack_driver(osp_cred, tenant)
-            instances = driver.list_nodes()
-            volumes = driver.list_volumes()
-            instances_dict = dict()
-            for instance in instances:
-                if instance.state == "error":
-                    p.spawn(
-                        instance_cleanup,
-                        instance,
-                        error_results,
-                        volumes,
-                        driver,
-                        instances_dict,
-                    )
-            dict_instance[tenant] = instances_dict
-    send_email_of_remove_instance(dict_instance, error=True)
-    return error_results
+    Sends an email to all recipients given in the payload.
 
+    Arguments:
+        payload     A dictionary having the primary key as email id
 
-def cleanup_instances_older_than_ttl(osp_cred):
-    """
-    Removes instance that are older than its time spam and its
-    volumes if any and Send email about the removed instances
-    :param:
-       - osp_cred: openstack details
-    :return: ttl_results:contains list of instance name in case of failure else empty list
-    """
-    ttl_results = []
-    dict_instance = dict()
-    projects = ["ceph-ci", "ceph-core", "ceph-jenkins"]
-    with parallel() as p:
-        for tenant in projects:
-            ttl = 3 if tenant == "ceph-jenkins" else 14
-            print(
-                f"Removing instance that are older than {ttl} days from Project:{tenant}"
-            )
-            driver = get_instances_from_openstack_driver(osp_cred, tenant)
-            instances = driver.list_nodes()
-            volumes = driver.list_volumes()
-            instances_dict = dict()
-            for instance in instances:
-                if "Do-Not-Delete".lower() not in instance.name.lower():
-                    age = str(datetime.now(timezone.utc) - instance.created_at)
-                    if "days" in age and int(age.split("days")[0]) > ttl:
-                        p.spawn(
-                            instance_cleanup,
-                            instance,
-                            ttl_results,
-                            volumes,
-                            driver,
-                            instances_dict,
-                        )
-            dict_instance[tenant] = instances_dict
-    send_email_of_remove_instance(dict_instance, error=False)
-    return ttl_results
+    Returns:
+        None
 
-
-def send_email_of_remove_instance(instance_dict, error):
-    """
-    This Method is for sending email that contains table of removed instances,
-    its project and instance creation date
-    :param:
-       - instance_dict: openstack details
-       - error: bool- true for instance with error state, False for instance with expired subscription
+    payload example
+        {
+            "abc@xyz.com": {
+                                "warn": { "ceph-ci": ["vm1", "vm2"] },
+                                "deleted": { "ceph-jenkins": ["vm3"] },
+                                "marked": { "ceph-core": ["vm10"] }
+                            },
+            "abc@xyz.com": {
+                                "warn": { "ceph-jenkins": [...]},
+                            },
     """
     sender = "cephci@redhat.com"
-    recipient = "cephci@redhat.com"
-    msg = MIMEMultipart("alternative")
-    reason = "it's Error state" if error else "it's expired VM subscription"
+    for email in payload.keys():
+        recipient = email
 
-    msg["Subject"] = f"Removed instances due to {reason}"
-    msg["From"] = sender
-    msg["To"] = recipient
-    html = """\
-        <html><head>
-          <style>table, th, td {border: 1px solid black; }</style></head>
-          <body><p>Hi Team,</p>
-            <p>Please find the bellow table which contains the summary of removal of instance</p>
-            </table><h4>Summary for removal of instance From Project</h4><table>
+        msg = MIMEMultipart("alternative")
+        msg["Subject"] = "Notification: Your subscriptions are marked/removed in RHOS-D"
+        msg["From"] = sender
+        msg["To"] = recipient
+
+        html = """\
+            <html>
+                <head>
+                    <style>table, th, td {border: 1px solid black; }</style>
+                </head>
+                <body>
+                    <p>Hi Team,</p>
+                    <p>
+                        The below cloud subscriptions have expired or expiring.
+                    </p>
+                    </table>
+                    <h4>Summary for removal of instance From Project</h4><table>
               <tr><th>Project</th><th>Instance name</th><th>Created On</th></tr>"""
-    for pro, instance in instance_dict.items():
-        no_of_instance = len(instance)
-        if no_of_instance == 0:
-            html += f"<tr><td>{pro}</td><td></td><td></td></tr>"
-        else:
-            html += f"<tr><td rowspan = '{no_of_instance}'>{pro}</td>"
-        for inst, con in instance.items():
-            if inst != list(instance.keys())[0]:
-                html += "<tr>"
-            html += f"<td>{inst}</td>"
-            html += f"<td>{con}</td></tr>"
-    html += "</table></body></html>"
-    part1 = MIMEText(html, "html")
-    msg.attach(part1)
-    s = smtplib.SMTP("localhost")
-    s.sendmail(sender, recipient, msg.as_string())
-    s.quit()
 
 
 @retry(LibcloudError, tries=5, delay=15)
-def run(args):
+def run(args: Dict) -> int:
     """
-    Method to remove instances that are in error state and instance that
-    are expired its subscription along with volumes if there is any
-    :param:
-       - args: required arguments for removal of instance
-    :return: 0 on success, 1 for failures
+    Using the provided credential file, this method removes the instances that are
+    running passed the allowable duration.
+
+    Arguments:
+        args: Dict - containing the key/value pairs passed by the user
+
+    Returns:
+        0 on success or 1 for failures
     """
+    global OSP_IDENTITY
+
     osp_cred_file = args["--osp-cred"]
-    if osp_cred_file:
-        with open(osp_cred_file, "r") as osp_cred_stream:
-            osp_cred = yaml.safe_load(osp_cred_stream)
-    error_result = cleanup_instances_with_error(osp_cred)
-    if len(error_result) > 0:
-        print(f"\nFailed to delete instances with error state: {error_result}")
-    ttl_result = cleanup_instances_older_than_ttl(osp_cred)
-    if len(ttl_result) > 0:
-        print(
-            f"\nFailed to delete instances with time to live greater the specified ttl: {ttl_result}"
-        )
-    error_result.extend(ttl_result)
-    if error_result:
-        return 1
-    return 0
+
+    with open(osp_cred_file, "r") as osp_cred_stream:
+        yh = yaml.safe_load(osp_cred_stream)
+        osp_cred = yh["globals"]["openstack-credentials"]
+
+        results = dict()
+
+        tenants = ["ceph-ci", "ceph-core", "ceph-jenkins"]
+        for tenant in tenants:
+            driver_ = get_driver(Provider.OPENSTACK)
+            osp_driver = driver_(
+                osp_cred["username"],
+                osp_cred["password"],
+                api_version="2.2",
+                ex_force_auth_url=osp_cred["auth-url"],
+                ex_force_auth_version=osp_cred["auth-version"],
+                ex_tenant_name=tenant,
+                ex_force_service_region=osp_cred["service-region"],
+                ex_domain_name=osp_cred["domain"],
+                ex_tenant_domain_id=osp_cred["tenant-domain-id"],
+            )
+
+            OSP_IDENTITY = osp_driver.connection.get_auth_class()
+            OSP_IDENTITY.connect()
+
+            with parallel() as p:
+                for node in osp_driver.list_nodes():
+                    p.spawn(cleanup, osp_cred, node, results, tenant)
+
+    # send_email(results)
+
+    return 1 if results["marked"] else 0
 
 
 if __name__ == "__main__":
-    args = docopt(doc)
-    rc = run(args)
+    arguments = docopt(doc)
+    rc = run(arguments)
     sys.exit(rc)
