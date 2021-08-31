5e5cead5af1026a3b96473f2b7745f8e29b91e12
ckulal <ckulal@redhat.com>
2021-08-31 09:13:09 +0530

    Porting sendemail and gchat notification method
    Signed-off-by: ckulal <ckulal@redhat.com>

--- pipeline/vars/lib.groovy
+++ pipeline/vars/lib.groovy
@@ -37,6 +37,133 @@ def prepareNode() {
     sh (script: "bash ${env.WORKSPACE}/pipeline/vars/node_bootstrap.bash")
 }
 
+def getCvpVariable() {
+    /*
+        Returns the cvp variable after processing the CI message
+    */
+    def ciMessage = "${params.CI_MESSAGE}" ?: ""
+    if (ciMessage?.trim() ) {
+        def ciMap = getCIMessageMap()
+        if (ciMap.containsKey("CVP")) {
+            return ciMap.CVP
+        }
+    }
+    return false
+}
+
+def sendGChatNotification(def tier){
+    /*
+        Send a GChat notification.
+        Plugin used:
+            googlechatnotification which allows to post build notifications to a Google Chat Messenger groups.
+            parameter:
+                url: Mandatory String parameter.
+                     Single/multiple comma separated HTTP URLs or/and single/multiple comma separated Credential IDs.
+                message: Mandatory String parameter.
+                         Notification message to be sent.
+    */
+
+    currentBuild.result = currentBuild.currentResult
+    def msg= "Run for ${env.composeId}:${tier} is ${currentBuild.result}. log:${env.BUILD_URL}"
+    googlechatnotification(url: "id:rhcephCIGChatRoom",
+                           message: msg
+                          )
+}
+
+def jsonToMap(def jsonFile) {
+    /*
+        Read the JSON file and returns a map object
+    */
+    def props = readJSON file: jsonFile
+    return props
+}
+
+def fetchEmailBodyAndReceiver(def testResults, def isStage) {
+    /*
+        Return the Email body with the the test results in a tabular form.
+    */
+    def toList = "ceph-qe-list@redhat.com"
+    def jobStatus = "STABLE"
+    def failureCount = 0
+    def body = "<table>"
+
+    if(isStage) {
+        body += "<tr><th>Test Suite</th><th>Result</th>"
+        for (test in testResults) {
+            def res = "PASS"
+            if (test.value != 0) {
+                res = "FAIL"
+                failureCount += 1
+            }
+
+            body += "<tr><td>${test.key}</td><td>${res}</td></tr>"
+        }
+    }
+    else {
+        body += "<tr><th>Jenkins Job Name</th><th>Result</th>"
+        for (test in testResults) {
+            if (test.value != "SUCCESS") {
+                failureCount += 1
+            }
+
+            body += "<tr><td>${test.key}</td><td>${test.value}</td></tr>"
+        }
+    }
+
+    body +="</table> </body> </html>"
+
+    if (failureCount > 0) {
+        toList = "cephci@redhat.com"
+        jobStatus = 'UNSTABLE'
+    }
+    return ["to_list" : toList, "jobStatus" : jobStatus, "body" : body]
+}
+
+def sendEMail(def subjectPrefix, def testResults, def isStage=true) {
+    /*
+        Send an email notification.
+    */
+    def versionFileExists = sh(
+        returnStatus: true, script: "ls -l version_info.json"
+    )
+    def versionInfo = [:]
+    def body = readFile(file: "pipeline/vars/emailable-report.html")
+    body += "<h2><u>Test Artifacts</h2></u><table><tr><td> COMPOSE_URL </td><td>${env.composeUrl}</td></tr><td>COMPOSE_ID</td><td> ${env.composeId}</td></tr>"
+    body += "<tr><td> REPOSITORY </td><td>${env.repository}</td></tr>"
+    if (versionFileExists == 0) {
+        versionInfo = jsonToMap("version_info.json")
+        for (def key in versionInfo.keySet()) {
+            body += "<tr><td> ${key} </td><td> ${versionInfo[key]}</td></tr>"
+        }
+    }
+    body += "</table>"
+    body += "<body><u><h3>Test Summary</h3></u><br />"
+    if (getCvpVariable()) {
+        def ciMsg = getCIMessageMap()
+        body += "<p>CVP Image : ${ciMsg.artifact.nvr}</p><br />"
+    }
+    body += "<p>Logs are available at ${env.BUILD_URL}</p><br />"
+    def params = fetchEmailBodyAndReceiver(testResults, isStage)
+    body += params["body"]
+
+    def toList = params["to_list"]
+    def jobStatus = params["jobStatus"]
+    def rh_ceph_version = env.rhcephVersion.substring(0,3)
+
+    def subject = "Test report status of RH Ceph ${rh_ceph_version} for ${subjectPrefix} is ${jobStatus}"
+    if (getCvpVariable()) {
+        subject = "${subjectPrefix} test execution is ${jobStatus}."
+    }
+
+    emailext (
+        mimeType: 'text/html',
+        subject: "${subject}",
+        body: "${body}",
+        from: "cephci@redhat.com",
+        to: "${toList}"
+    )
+}
+
 def yamlToMap(def yamlFile, def location="/ceph/cephci-jenkins/latest-rhceph-container-info") {
     /*
         Read the yaml file and returns a map object
