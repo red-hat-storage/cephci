d72bb5af43184a380dac432303ec7b3c85efd693
sunilkumarn417 <sunnagar@redhat.com>
2022-01-11 12:11:16 +0530

    code changes for cephadm-ansible
    Signed-off-by: sunilkumarn417 <sunnagar@redhat.com>

--- tests/cephadm/test_client.py
+++ tests/cephadm/test_client.py
@@ -9,18 +9,20 @@ log = logging.getLogger(__name__)
 
 
 def add(cls, config: Dict) -> None:
-    """
-    configure client using the provided configuration.
+    """configure client using the provided configuration.
 
     Args:
         cls: cephadm object
         config: Key/value pairs provided by the test case to create the client.
 
-    Example
+    Example::
+
         config:
             command: add
             id: client.1                    # client Id
             node: "node8"                   # client node
+            copy_ceph_conf: true|false      # copy ceph conf to provided node
+            store-keyring: true             # store keyrin locally under /etc/ceph
             install_packages:
               - ceph_common                 # install ceph common packages
             copy_admin_keyring: true|false  # copy admin keyring
@@ -29,40 +31,47 @@ def add(cls, config: Dict) -> None:
               - "osd 'allow rw pool=liverpool'"
     """
     id_ = config["id"]
-    node = get_node_by_id(cls.cluster, config["node"])
-
-    def put_file(client, file_name, content, file_mode, sudo=True):
-        file_ = client.remote_file(sudo=sudo, file_name=file_name, file_mode=file_mode)
-        file_.write(content)
-        file_.flush()
+    client_file = f"/etc/ceph/ceph.{id_}.keyring"
 
     # Create client
     cmd = ["ceph", "auth", "get-or-create", f"{id_}"]
     [cmd.append(f"{k} '{v}'") for k, v in config.get("caps", {}).items()]
     cnt_key, err = cls.shell(args=cmd)
 
-    # Copy the keyring to client
-    node.exec_command(sudo=True, cmd="mkdir -p /etc/ceph")
-    put_file(node, f"/etc/ceph/ceph.{id_}.keyring", cnt_key, "w")
+    def put_file(client, file_name, content, file_mode, sudo=True):
+        file_ = client.remote_file(sudo=sudo, file_name=file_name, file_mode=file_mode)
+        file_.write(content)
+        file_.flush()
 
-    # Get minimal ceph.conf
-    ceph_conf, err = cls.shell(args=["ceph", "config", "generate-minimal-conf"])
+    if config.get("node"):
+        node = get_node_by_id(cls.cluster, config["node"])
 
-    # Copy the ceph.conf to client
-    put_file(node, "/etc/ceph/ceph.conf", ceph_conf, "w")
+        # Copy the keyring to client
+        node.exec_command(sudo=True, cmd="mkdir -p /etc/ceph")
+        put_file(node, client_file, cnt_key, "w")
 
-    # Copy admin keyring to client node
-    if config.get("copy_admin_keyring"):
-        admin_keyring, _ = cls.shell(args=["ceph", "auth", "get", "client.admin"])
-        put_file(node, "/etc/ceph/ceph.client.admin.keyring", admin_keyring, "w")
+        if config.get("copy_ceph_conf", True):
+            # Get minimal ceph.conf
+            ceph_conf, err = cls.shell(args=["ceph", "config", "generate-minimal-conf"])
+            # Copy the ceph.conf to client
+            put_file(node, "/etc/ceph/ceph.conf", ceph_conf, "w")
 
-    # Install ceph-common
-    if config.get("install_packages"):
-        for pkg in config.get("install_packages"):
-            node.exec_command(cmd=f"yum install -y --nogpgcheck {pkg}", sudo=True)
+        # Copy admin keyring to client node
+        if config.get("copy_admin_keyring"):
+            admin_keyring, _ = cls.shell(args=["ceph", "auth", "get", "client.admin"])
+            put_file(node, "/etc/ceph/ceph.client.admin.keyring", admin_keyring, "w")
 
-    out, _ = node.exec_command(cmd="ls -ltrh /etc/ceph/", sudo=True)
-    log.info(out.read().decode().strip())
+        # Install ceph-common
+        if config.get("install_packages"):
+            for pkg in config.get("install_packages"):
+                node.exec_command(cmd=f"yum install -y --nogpgcheck {pkg}", sudo=True)
+
+        out, _ = node.exec_command(cmd="ls -ltrh /etc/ceph/", sudo=True)
+        log.info(out.read().decode().strip())
+
+    # Hold local copy of the client key-ring in the installer node
+    if config.get("store-keyring"):
+        put_file(cls.installer, client_file, cnt_key, "w")
 
 
 def remove(cls, config: Dict) -> None:
@@ -73,7 +82,8 @@ def remove(cls, config: Dict) -> None:
         cls: cephadm object
         config: Key/value pairs provided by the test case to create the client.
 
-    Example
+    Example::
+
         config:
             command: remove
             id: client.0                # client Id
