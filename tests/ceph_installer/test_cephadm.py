72aaeeb10bb47d9a61b0436c32ae420d4473bfdf
Anuchaithra Rao <anrao@redhat.com>
2022-01-27 12:41:58 +0530

    [Logging][RGW] Migrate to use custom Log class - Tier-0 suites 5.x
    Signed-off-by: Anuchaithra Rao <anrao@redhat.com>

--- tests/ceph_installer/test_cephadm.py
+++ tests/ceph_installer/test_cephadm.py
@@ -4,11 +4,9 @@ Test suite that verifies the deployment of RedHat Ceph Storage via the cephadm C
 The intent of the suite is to simulate a standard operating procedure expected by a
 customer.
 """
-import logging
-
 from ceph.ceph import Ceph
 from ceph.ceph_admin import CephAdmin
-from ceph.ceph_admin.alert_manager import AlertManager
+from ceph.ceph_admin.alert_manager import AlertManagers
 from ceph.ceph_admin.cephfs_mirror import CephfsMirror
 from ceph.ceph_admin.common import fetch_method
 from ceph.ceph_admin.crash import Crash
@@ -27,8 +25,9 @@ from ceph.ceph_admin.osd import OSD
 from ceph.ceph_admin.prometheus import Prometheus
 from ceph.ceph_admin.rbd_mirror import RbdMirror
 from ceph.ceph_admin.rgw import RGW
+from utility.log import Log
 
-LOG = logging.getLogger()
+LOG = Log(__name__)
 
 
 SERVICE_MAP = dict(
