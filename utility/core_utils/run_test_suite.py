cb92769fd7179b6bba848b9d91db0a841348c509
Hemanth Maheswarla <hmaheswa@hmaheswa.remote.csb>
2022-06-24 14:51:16 +0530

    modified run_test_suite
    Signed-off-by: Hemanth Maheswarla <hmaheswa@hmaheswa.remote.csb>

--- utility/core_utils/run_test_suite.py
+++ utility/core_utils/run_test_suite.py
@@ -4,7 +4,7 @@ import importlib
 import logging
 import os
 import sys
-from keyword import iskeyword
+from rhcs_qe_sdk.src.rhcs_qe_sdk import tests
 
 from gevent import monkey
 
@@ -137,53 +137,7 @@ class RunTestSuite:
         # self.cli_obj = CLI()
         self.suite_config = LoaderMixin().load_file(config, kind="suites")
         self.parallel_executor = ParallelExecutor()
-
-    def redefine_service(self, method):
-        """
-        This method is used to rename the inbuilt method by concatenating underscore.
-        Args:
-          method(str): Takes inbuilt method name as input.
-
-        Returns:
-          method(str): Modified method name.
-
-        Example: changes the inbuilt keyword 'import' to 'import_'.
-        """
-        if "-" in str(method):
-            method = str(method).replace("-", "_")
-        if self.check_for_builtins(method):
-            method = method + "_"
-        return method
-
-    def check_for_builtins(self, service):
-        """
-        This method is used to check whether the service is a inbuilt function.
-        If it is an inbuilt function it is modified.
-        Args:
-          service(str): name of the method
-
-        Returns:
-          None
-        """
-        if iskeyword(service) or service in dir(builtins):
-            return True
-        else:
-            for key in builtins.__dict__.keys():
-                if service in dir(builtins.__dict__[key]):
-                    return True
-            return False
-
-    def get_must_key(self, test_config):
-        """
-        This method is used to check whether test has to pass or fail or raise an exception mandatorily.
-        Args:
-          test_config(Dict): dictionary consists of test configuration in key-value pair.
-        Returns:
-          must_present_key(key): a key [must_pass | must_fail ]
-        """
-        for must_present_key in METHOD_MAP.keys():
-            if test_config.get(must_present_key):
-                return must_present_key
+        self.jenkins_rc = 0
 
     def run_tests(self, tests):
         """
@@ -194,126 +148,121 @@ class RunTestSuite:
         Returns:
           None
         """
-        return self.parallel_executor.run_until_complete((self.run_tests_async, tests))
-
-    async def run_tests_async(self, tests):
-        """
-        This method is used to run list of tests present in a yaml file.
-        Args:
-          None
-
-        Returns:
-          None
-        """
         logger.info("Running test suite")
         logger.info(f"Running test {self.config}")
-        jenkins_rc = 0
+        self.jenkins_rc = 0
         for test in tests:
             if test.get("parallel"):
                 with parallel() as p:
                     for test_pll in test.get("parallel"):
-                        p.spawn(self.run_tests_async, test_pll)
-            test = test.get("test")
-            tc = self.fetch_test_details(test)
-            report_portal_description = test.get("desc") or ""
-            unique_test_name = create_unique_test_name(
-                test.get("name"), self.test_names
+                        p.spawn(self.run_tests, test_pll)
+            if not self.run_test(test):
+                break
+
+        return self.jenkins_rc
+
+    def run_test(self, test):
+        test = test.get("test")
+        tc = self.fetch_test_details(test)
+        report_portal_description = test.get("desc") or ""
+        unique_test_name = create_unique_test_name(
+            test.get("name"), self.test_names
+        )
+        self.test_names.append(unique_test_name)
+        tc["log-link"] = configure_logger(unique_test_name, self.run_dir)
+        test_data = test.get("test_data")
+        module = test.get("module")
+        module = importlib.import_module(module, package=tests)
+        tc = test
+        tc["log-link"] = configure_logger(unique_test_name, self.run_dir)
+        runs_on = test.get("runs_on", [None])
+        logger.info(f"Running test {test_data}")
+        start = datetime.datetime.now()
+        if runs_on:
+            with parallel() as p:
+                for cluster_name in runs_on:
+                    p.spawn(module, test_data, cluster_name, self.osp_cred, self.ceph_cluster_dict)
+        for out in p:
+            cluster_name = out.get("cluster_name")
+            rc = out.get("rc")
+            try:
+                if self.post_to_report_portal:
+                    self.rp_logger.start_test_item(
+                        name=unique_test_name,
+                        description=report_portal_description,
+                        item_type="STEP",
+                    )
+                    self.rp_logger.log(
+                        message=f"Logfile location - {tc['log-link']}"
+                    )
+                    self.rp_logger.log(message=f"Polarion ID: {tc['polarion-id']}")
+
+                # Initialize the cluster with the expected rhcs_version hence the
+                # precedence would be from test suite.
+                # rhbuild would start with the version for example 5.0 or 4.2-rhel-7
+                _rhcs_version = test.get("ceph_rhcs_version", self.rhbuild[:3])
+                self.ceph_cluster_dict[cluster_name].rhcs_version = _rhcs_version
+            except BaseException as be:  # noqa
+                logger.exception(be)
+                rc = 1
+            finally:
+                collect_recipe(self.ceph_cluster_dict[cluster_name])
+                if self.store:
+                    store_cluster_state(
+                        self.ceph_cluster_dict, self.ceph_clusters_file
+                    )
+
+            if rc != 0:
+                return False
+
+        elapsed = datetime.datetime.now() - start
+        tc["duration"] = elapsed
+
+        # Write to report portal
+        if self.post_to_report_portal:
+            self.rp_logger.finish_test_item(
+                status="PASSED" if rc == 0 else "FAILED"
             )
-            self.test_names.append(unique_test_name)
-            tc["log-link"] = configure_logger(unique_test_name, self.run_dir)
-            test_data = test.get("test_data")
-            module = test.get("module")
-            module = importlib.import_module(module)
-            tc = test
-            tc["log-link"] = configure_logger(unique_test_name, self.run_dir)
-            runs_on = test.get("runs_on", [None])
-            logger.info(f"Running test {test_data}")
-            start = datetime.datetime.now()
-            if runs_on:
-                with parallel() as p:
-                    for cluster_name in runs_on:
-                        p.spawn(module, test_data, cluster_name)
-            for out in p:
-                cluster_name = out.get("cluster_name")
-                rc = out.get("rc")
-                try:
-                    if self.post_to_report_portal:
-                        self.rp_logger.start_test_item(
-                            name=unique_test_name,
-                            description=report_portal_description,
-                            item_type="STEP",
-                        )
-                        self.rp_logger.log(
-                            message=f"Logfile location - {tc['log-link']}"
-                        )
-                        self.rp_logger.log(message=f"Polarion ID: {tc['polarion-id']}")
 
-                    # Initialize the cluster with the expected rhcs_version hence the
-                    # precedence would be from test suite.
-                    # rhbuild would start with the version for example 5.0 or 4.2-rhel-7
-                    _rhcs_version = test.get("ceph_rhcs_version", self.rhbuild[:3])
-                    self.ceph_cluster_dict[cluster_name].rhcs_version = _rhcs_version
-                except BaseException as be:  # noqa
-                    logger.exception(be)
-                    rc = 1
-                finally:
-                    collect_recipe(self.ceph_cluster_dict[cluster_name])
-                    if self.store:
-                        store_cluster_state(
-                            self.ceph_cluster_dict, self.ceph_clusters_file
-                        )
+        if rc == 0:
+            tc["status"] = "Pass"
+            msg = "Test {} passed".format(test_data)
+            logger.info(msg)
+            print(msg)
 
-                if rc != 0:
-                    break
+            if self.post_results:
+                post_to_polarion(tc=tc)
+        else:
+            tc["status"] = "Failed"
+            msg = "Test {} failed".format(test_data)
+            logger.info(msg)
+            print(msg)
+            self.jenkins_rc = 1
 
-            elapsed = datetime.datetime.now() - start
-            tc["duration"] = elapsed
+            if self.post_results:
+                post_to_polarion(tc=tc)
 
-            # Write to report portal
-            if self.post_to_report_portal:
-                self.rp_logger.finish_test_item(
-                    status="PASSED" if rc == 0 else "FAILED"
-                )
+            if test.get("abort-on-fail", False):
+                logger.info("Aborting on test failure")
+                self.tcs.append(tc)
+                return False
 
-            if rc == 0:
-                tc["status"] = "Pass"
-                msg = "Test {} passed".format(test_data)
-                logger.info(msg)
-                print(msg)
+        if test.get("destroy-cluster") is True:
+            if self.cloud_type == "openstack":
+                cleanup_ceph_nodes(self.osp_cred, self.instances_name)
+            elif self.cloud_type == "ibmc":
+                cleanup_ibmc_ceph_nodes(self.osp_cred, self.instances_name)
 
-                if self.post_results:
-                    post_to_polarion(tc=tc)
-            else:
-                tc["status"] = "Failed"
-                msg = "Test {} failed".format(test_data)
-                logger.info(msg)
-                print(msg)
-                jenkins_rc = 1
-
-                if self.post_results:
-                    post_to_polarion(tc=tc)
-
-                if test.get("abort-on-fail", False):
-                    logger.info("Aborting on test failure")
-                    self.tcs.append(tc)
-                    break
-
-            if test.get("destroy-cluster") is True:
-                if self.cloud_type == "openstack":
-                    cleanup_ceph_nodes(self.osp_cred, self.instances_name)
-                elif self.cloud_type == "ibmc":
-                    cleanup_ibmc_ceph_nodes(self.osp_cred, self.instances_name)
-
-            if test.get("recreate-cluster") is True:
-                ceph_cluster_dict, clients = create_nodes(
-                    self.conf,
-                    self.inventory,
-                    self.osp_cred,
-                    self.run_id,
-                    self.cloud_type,
-                    self.service,
-                    self.instances_name,
-                    enable_eus=self.enable_eus,
-                )
-            self.tcs.append(tc)
-        return jenkins_rc
+        if test.get("recreate-cluster") is True:
+            self.ceph_cluster_dict, clients = create_nodes(
+                self.conf,
+                self.inventory,
+                self.osp_cred,
+                self.run_id,
+                self.cloud_type,
+                self.service,
+                self.instances_name,
+                enable_eus=self.enable_eus,
+            )
+        self.tcs.append(tc)
+        return True
