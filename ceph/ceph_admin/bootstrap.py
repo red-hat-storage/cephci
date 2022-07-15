9bc115f362f0e6b001547993ead5bce7c3d9708e
sayaleeraut <saraut@redhat.com>
2022-07-15 15:02:38 +0530

    Adding coverage for version 6.0
    Signed-off-by: sayaleeraut <saraut@redhat.com>

--- ceph/ceph_admin/bootstrap.py
+++ ceph/ceph_admin/bootstrap.py
@@ -275,7 +275,7 @@ class BootstrapMixin:
 
         # Todo: This patch is specific to 5.1 release,
         #   should be removed for next 5.x development builds or release.
-        if rhbuild.split("-")[0] in ["5.1", "5.2"]:
+        if rhbuild.split("-")[0] in ["5.1", "5.2", "6.0"]:
             cmd += " --yes-i-know"
 
         out, err = self.installer.exec_command(
