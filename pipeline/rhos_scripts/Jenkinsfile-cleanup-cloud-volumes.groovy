5c604bc6573cf75b4a084745a6f7f398219e3569
Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>
2022-07-17 18:09:03 +0530

    Migrating scripts to use the new location and library
    Signed-off-by: Pragadeeswaran Sathyanarayanan <psathyan@redhat.com>

--- pipeline/rhos_scripts/Jenkinsfile-cleanup-cloud-volumes.groovy
+++ pipeline/rhos_scripts/Jenkinsfile-cleanup-cloud-volumes.groovy
@@ -2,38 +2,49 @@
     Pipeline script for clean up of orphaned volumes in rhos-d environment.
 */
 // Global variables section
-
-def nodeName = "centos-7"
 def sharedLib
 
-node(nodeName){
-    stage('Install prereq') {
-        checkout([
-            $class: 'GitSCM',
-            branches: [[name: 'origin/master']],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [[
-                $class: 'SubmoduleOption',
-                disableSubmodules: false,
-                parentCredentials: false,
-                recursiveSubmodules: true,
-                reference: '',
-                trackingSubmodules: false
-            ]],
-            submoduleCfg: [],
-            userRemoteConfigs: [[url: 'https://github.com/red-hat-storage/cephci.git']]
-        ])
+node("rhel-8-medium") {
+
+    stage('prepareNode') {
+        checkout(
+                scm: [
+                    $class: 'GitSCM',
+                    branches: [[name: "origin/master"]],
+                    extensions: [[
+                        $class: 'CleanBeforeCheckout',
+                        deleteUntrackedNestedRepositories: true
+                    ], [
+                        $class: 'WipeWorkspace'
+                    ], [
+                        $class: 'CloneOption',
+                        depth: 1,
+                        noTags: true,
+                        shallow: true,
+                        timeout: 10,
+                        reference: ''
+                    ]],
+                    userRemoteConfigs: [[
+                        url: "https://github.com/red-hat-storage/cephci.git"
+                    ]]
+                ],
+                changelog: false,
+                poll: false
+            )
         sharedLib = load("${env.WORKSPACE}/pipeline/vars/common.groovy")
         sharedLib.prepareNode()
     }
-    stage('Scrub RHOS-Ring') {
-        echo "Cleanup Orphaned cloud volumes  and Send Email to respective user"
-        cmd = "${env.WORKSPACE}/.venv/bin/python ${env.WORKSPACE}/utility/cleanup_cloud_volumes.py --osp-cred ${env.HOME}/osp-cred-ci-2.yaml"
-        echo cmd
+
+    stage('cleanUp') {
+        println("Cleanup Orphaned cloud volumes  and Send Email to respective user")
+        cmd = "${env.WORKSPACE}/.venv/bin/python"
+        cmd += " ${env.WORKSPACE}/utility/cleanup_cloud_volumes.py"
+        cmd += " --osp-cred ${env.HOME}/osp-cred-ci-2.yaml"
+        println(cmd)
+
         rc = sh(script: "${cmd}", returnStatus: true)
-        if (rc != 0)
-        {
-            sh "echo \"stage failed with exit code : ${rc}\""
+        if (rc != 0) {
+            println("Something went wrong... ${rc}")
             currentBuild.result = 'FAILURE'
         }
     }
--- pipeline/rhos_scripts/Jenkinsfile-cleanup-env.groovy
+++ pipeline/rhos_scripts/Jenkinsfile-cleanup-env.groovy
@@ -2,61 +2,70 @@
     Pipeline script for clean up rhos-d environment.
 */
 // Global variables section
-
-def nodeName = "centos-7"
 def sharedLib
 
-node(nodeName){
+node("rhel-8-medium") {
     try {
-        stage('Install prereq') {
-            checkout([
-                $class: 'GitSCM',
-                branches: [[name: '*/master']],
-                doGenerateSubmoduleConfigurations: false,
-                extensions: [[
-                    $class: 'SubmoduleOption',
-                    disableSubmodules: false,
-                    parentCredentials: false,
-                    recursiveSubmodules: true,
-                    reference: '',
-                    trackingSubmodules: false
-                ]],
-                submoduleCfg: [],
-                userRemoteConfigs: [[url: 'https://github.com/red-hat-storage/cephci.git']]
-            ])
-            sharedLib = load("${env.WORKSPACE}/pipeline/vars/common.groovy")
+        stage('prepareNode') {
+            checkout(
+                scm: [
+                    $class: 'GitSCM',
+                    branches: [[name: "origin/master"]],
+                    extensions: [[
+                        $class: 'CleanBeforeCheckout',
+                        deleteUntrackedNestedRepositories: true
+                    ], [
+                        $class: 'WipeWorkspace'
+                    ], [
+                        $class: 'CloneOption',
+                        depth: 1,
+                        noTags: true,
+                        shallow: true,
+                        timeout: 10,
+                        reference: ''
+                    ]],
+                    userRemoteConfigs: [[
+                        url: "https://github.com/red-hat-storage/cephci.git"
+                    ]]
+                ],
+                changelog: false,
+                poll: false
+            )
+            sharedLib = load("${env.WORKSPACE}/pipeline/vars/v3.groovy")
             sharedLib.prepareNode()
         }
-        stage('Scrub RHOS-Ring') {
-            echo "Cleanup environment and Send Email to respective user"
-            cmd = "${env.WORKSPACE}/.venv/bin/python ${env.WORKSPACE}/cleanup_env.py --osp-cred ${env.HOME}/osp-cred-ci-2.yaml"
-            echo cmd
+        stage('cleanup') {
+            println("Cleanup environment and Send Email to respective user")
+            cmd = "${env.WORKSPACE}/.venv/bin/python ${env.WORKSPACE}/utility/psi_remove_vms.py --osp-cred ${env.HOME}/osp-cred-ci-2.yaml"
+            println cmd
+
             rc = sh(script: "${cmd}", returnStatus: true)
-            if (rc != 0)
-            {
-                sh "echo \"stage failed with exit code : ${rc}\""
+            if (rc != 0) {
+                println("Something went wrong... ${rc}")
                 currentBuild.result = 'FAILURE'
             }
         }
     } catch(Exception err) {
-        if (currentBuild.result != "ABORTED") {
-            // notify about failure
-            currentBuild.result = "FAILURE"
-            def failureReason = err.getMessage()
-            def subject =  "[CEPHCI-PIPELINE-ALERT] [JOB-FAILURE] - ${env.JOB_NAME}/${env.BUILD_NUMBER}"
-            def body = "<body><h3><u>Job Failure</u></h3></p>"
-            body += "<dl><dt>Jenkins Build:</dt><dd>${env.BUILD_URL}</dd>"
-            body += "<dt>Failure Reason:</dt><dd>${failureReason}</dd></dl></body>"
-
-            emailext (
-                mimeType: 'text/html',
-                subject: "${subject}",
-                body: "${body}",
-                from: "cephci@redhat.com",
-                to: "cephci@redhat.com"
-            )
-            subject += "\n Jenkins URL: ${env.BUILD_URL}"
-            googlechatnotification(url: "id:rhcephCIGChatRoom", message: subject)
+        if (currentBuild.result == "ABORTED") {
+            println("Aborting the workflow")
         }
+
+        // notify about failure
+        currentBuild.result = "FAILURE"
+        def failureReason = err.getMessage()
+        def subject =  "[CEPHCI-PIPELINE-ALERT] [JOB-FAILURE] - ${env.JOB_NAME}/${env.BUILD_NUMBER}"
+        def body = "<body><h3><u>Job Failure</u></h3></p>"
+        body += "<dl><dt>Jenkins Build:</dt><dd>${env.BUILD_URL}</dd>"
+        body += "<dt>Failure Reason:</dt><dd>${failureReason}</dd></dl></body>"
+
+        emailext (
+            mimeType: 'text/html',
+            subject: "${subject}",
+            body: "${body}",
+            from: "cephci@redhat.com",
+            to: "cephci@redhat.com"
+        )
+        subject += "\n Jenkins URL: ${env.BUILD_URL}"
+        googlechatnotification(url: "id:rhcephCIGChatRoom", message: subject)
     }
 }
