68169af1e7807397ded4c3b1173d8b96778cb71f
mgowri <mgowri@redhat.com>
2022-08-08 16:32:53 +0530

    Changing default tolist to cephci@redhat.com
    Signed-off-by: mgowri <mgowri@redhat.com>

--- pipeline/vars/v3.groovy
+++ pipeline/vars/v3.groovy
@@ -41,7 +41,7 @@ def sendEmail(
     def artifactDetails,
     def tierLevel = null,
     def stageLevel = null,
-    def toList="ceph-qe-list@redhat.com"
+    def toList="cephci@redhat.com"
     ) {
     /*
         Send an Email
