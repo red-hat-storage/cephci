8cfccfae9bc1565e4b4a6bcaef09ba033df2e7be
mgowri <mgowri@redhat.com>
2022-11-02 12:01:40 +0530

    Fixing None type errors seen because of recent changes to getPipelineStages
    Signed-off-by: mgowri <mgowri@redhat.com>

--- pipeline/scripts/ci/getPipelineStages.py
+++ pipeline/scripts/ci/getPipelineStages.py
@@ -67,13 +67,14 @@ def fetch_stages(args):
     metadata_file = f"{metadata_dir}/{args['rhcephVersion']}.yaml"
     metadata_content = yaml.safe_load(open(metadata_file, "r"))
     metadata_overrides = metadata_content.get("overrides", {})
-    ibmc_overrides = metadata_overrides.pop("ibmc", None)
-    openstack_overrides = metadata_overrides.pop("openstack", None)
+    cloud_overrides = {
+        "ibmc": metadata_overrides.pop("ibmc", {}),
+        "openstack": metadata_overrides.pop("openstack", {}),
+    }
     data = metadata_content.get("suites")
     tags = args["tags"].split(",")
     tags_next = args["tags"].split(",")
     overrides = json.loads(args.get("overrides", {}))
-    overrides_log = json.loads(args.get("overrides", {}))
     cloud_type = "openstack"
 
     # filter metadata file for test suites based on tags provided
@@ -107,25 +108,22 @@ def fetch_stages(args):
         execute_cli = ".venv/bin/python run.py --osp-cred $HOME/osp-cred-ci-2.yaml"
         execute_cli += f" --instances-name {instances_name}"
         execute_cli += " --log-level DEBUG"
-        logdir = ""
-        script.update({"inventory": script["inventory"][cloud_type]})
+        workspace = overrides.pop("workspace", "")
+        build_number = overrides.pop("build_number", "")
+        logdir = f"{workspace}/logs/{generate_random_string(5)}-{build_number}"
+
         if "ibmc" in tags:
             cloud_type = "ibmc"
             execute_cli += " --xunit-results"
             execute_cli += " --cloud ibmc"
-            workspace = overrides_log.get("workspace")
-            build_number = overrides_log.get("build_number")
-            logdir = f"{workspace}/logs/{generate_random_string(5)}-{build_number}"
             os.makedirs(logdir)
             execute_cli += f" --log-dir {logdir}"
             cleanup_cli += f" --cloud {cloud_type}"
-            script.update(ibmc_overrides)
-        else:
-            script.update(openstack_overrides)
+
+        script.update({"inventory": script["inventory"][cloud_type]})
+        script.update(cloud_overrides[cloud_type])
 
         script.update(metadata_overrides)
-        overrides.pop("workspace", None)
-        overrides.pop("build_number", None)
         script.update(overrides)
 
         for (k, v) in script.items():
