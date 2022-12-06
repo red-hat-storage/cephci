a269d89cb33b3d8348066be9233674a605ef84b7
Mohit <mobisht@redhat.com>
2022-12-05 23:23:18 -0500

    [SDK][CephAdm]Adding ceph orch support
    Signed-off-by: Mohit <mobisht@redhat.com>

--- cli/cephadm/cephadm.py
+++ cli/cephadm/cephadm.py
@@ -5,11 +5,13 @@ from cli.ceph.ceph import Ceph
 class CephAdm(Cli):
     """This module provides CLI interface to manage the CephAdm operations."""
 
-    def __init__(self, nodes, base_cmd="cephadm"):
+    def __init__(self, nodes, mount=None, base_cmd="cephadm"):
         super(CephAdm, self).__init__(nodes)
 
         self.base_cmd = base_cmd
         self.base_shell_cmd = f"{self.base_cmd} shell"
+        if mount:
+            self.base_shell_cmd += f" --mount {mount} --"
 
         self.ceph = Ceph(nodes, self.base_shell_cmd)
 
