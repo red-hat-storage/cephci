2c1cb079c705600f91621a7cbcb5886644de64b9
Mohit <mobisht@redhat.com>
2023-03-08 04:54:51 -0500

    [Ceph-Mgr][Tier2]Verify mgr balancer
    Signed-off-by: Mohit <mobisht@redhat.com>

---
+++ cli/ceph/config_key.py
@@ -0,0 +1,19 @@
+from cli import Cli
+
+
+class ConfigKey(Cli):
+    """This module provides CLI interface to manage the balancer module."""
+
+    def __init__(self, nodes, base_cmd):
+        super(ConfigKey, self).__init__(nodes)
+
+        self.base_cmd = f"{base_cmd} config-key"
+
+    def set(self, key, value):
+        """set config key to value
+        Args:
+            key (str): config key
+            value (str): config key value
+        """
+        cmd = f"{self.base_cmd} set {key} {value}"
+        return self.execute(sudo=True, check_ec=False, long_running=True, cmd=cmd)
