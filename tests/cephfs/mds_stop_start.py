5945893bb5a2f6457b8be3a82037fd2fc243e948
yogesh-mane <ymane@redhat.com>
2023-04-06 17:13:28 +0530

    added standby CEPH-83574339 yogesh@Yogeshs-MacBook-Pro Signed-off-by: yoegsh <yogesh@Yogeshs-MacBook-Pro> Signed-off-by: ymane yogesh@Yogeshs-MacBook-Pro <ymane@redhat.com>
    Signed-off-by: yogesh-mane <ymane@redhat.com>

--- tests/cephfs/mds_stop_start.py
+++ tests/cephfs/mds_stop_start.py
@@ -4,12 +4,12 @@ import traceback
 from datetime import datetime, timedelta
 from time import sleep
 
-from ceph.ceph import CommandFailed
 from ceph.parallel import parallel
 from tests.cephfs.cephfs_utilsV1 import FsUtils
 from utility.log import Log
 
 log = Log(__name__)
+global stop_flag
 
 
 def start_io_time(fs_util, client1, mounting_dir, timeout=300):
@@ -30,75 +30,38 @@ def start_io_time(fs_util, client1, mounting_dir, timeout=300):
             client1, f"{mounting_dir}/run_ios_{iter}", io_tools=["smallfile"]
         )
         iter = iter + 1
+        log.info(f"IO stop flag {stop_flag}")
 
 
-def wait_for_cmd(client1, process_name, count, timeout=180, interval=5):
-    end_time = datetime.now() + timedelta(seconds=timeout)
-    log.info("Wait for the process to start or stop")
-    while end_time > datetime.now():
-        out, rc = client1.exec_command(
-            sudo=True,
-            cmd=f"ceph orch ps | grep {process_name} | wc -l ",
-            check_ec=False,
-        )
-        out2, rc = client1.exec_command(
-            sudo=True, cmd=f"ceph orch ps | grep {process_name} ", check_ec=False
-        )
-        print(out2)
-        output = out.rstrip()
-        log.info(f"Current mds count = {output}")
-        log.info(f"Expected mds count = {count}")
-        if output == count:
-            return 0
-        sleep(interval)
-    raise CommandFailed
-
-
-def mds_rm_add(fs_util, mdss, client):
-    try:
-        global stop_flag
-        log.info("removing mds")
-        mds_hosts = []
-        for mds_host in mdss:
-            mds_hosts.append(mds_host.node.hostname)
-        mdss_hosts = " ".join([str(elem) for elem in mds_hosts[:-1]])
-        initial_mds_count = str(len(mds_hosts))
-        initial_mds_count.rstrip()
-        count = str(int(initial_mds_count) - 1)
-        cmd = f"ceph orch  --verbose apply mds cephfs --placement='{mdss_hosts}'"
-        client.exec_command(sudo=True, cmd=cmd)
-        wait_for_cmd(client, "mds", count)
-        mdss_hosts = " ".join([str(elem) for elem in mds_hosts])
-        cmd = f"ceph orch  --verbose apply mds cephfs --placement='{mdss_hosts}'"
-        client.exec_command(sudo=True, cmd=cmd)
-        wait_for_cmd(client, "mds", initial_mds_count)
-        stop_flag = True
-
-    except Exception as e:
-        log.info(e)
-        log.info(traceback.format_exc())
-        raise CommandFailed
+def mds_stop_start(fs_util, mds_nodes):
+    global stop_flag
+    for mds in mds_nodes:
+        deamon_name = fs_util.deamon_name(mds, "mds")
+        fs_util.deamon_op(mds, "mds", "stop")
+        sleep(60)
+        fs_util.deamon_op(mds, "mds", "start", service_name=deamon_name)
+    stop_flag = True
 
 
 def run(ceph_cluster, get_fs_info=None, **kw):
     try:
         """
-        CEPH-11259 - MDS node/service add and removal test, with client
+        CEPH-83574339 - Test Start/Stop mds daemons
         Pre-requisites :
         1. Create cephfs volume
 
         Test Case Flow:
         1. Start IOs
-        2. remove 1 msd
+        2. Stop mds daemon
         3. IO's shouldn't fail
-        4. add 1 msd
+        4. Start mds daemon
         5. IO's shouldn't fail
 
         Cleanup:
         1. Remove all the data in cephfs
         2. Remove all the mounts
         """
-        tc = "CEPH-11259"
+        tc = "CEPH-83574339"
         log.info("Running cephfs %s test case" % (tc))
 
         fs_util = FsUtils(ceph_cluster)
@@ -116,8 +79,9 @@ def run(ceph_cluster, get_fs_info=None, **kw):
             [client1], kernel_mount_dir, mon_node_ip, new_client_hostname="admin"
         )
         fs_util.fuse_mount([client1], fuse_mount_dir, new_client_hostname="admin")
+        global stop_flag
         with parallel() as p:
-            p.spawn(mds_rm_add, fs_util, mdss, client1)
+            p.spawn(mds_stop_start, fs_util, mdss)
             p.spawn(
                 start_io_time,
                 fs_util,
@@ -133,7 +97,6 @@ def run(ceph_cluster, get_fs_info=None, **kw):
                 timeout=0,
             )
         return 0
-
     except Exception as e:
         log.error(e)
         log.error(traceback.format_exc())
