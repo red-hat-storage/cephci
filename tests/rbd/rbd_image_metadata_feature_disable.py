4b0ec7166f24f9df4d379dc9425f8f8cc5d54784
Rajendra Khambadkar <rkhambad@rkhambad.remote.csb>
2023-04-10 12:53:15 +0530

    CEPH-9864-Disable some existing Image Feature which have Image metadata set on it, with IO in progress from Client Side
    Signed-off-by: Rajendra Khambadkar <rkhambad@rkhambad.remote.csb>

---
+++ tests/rbd/rbd_image_metadata_feature_disable.py
@@ -0,0 +1,91 @@
+from ceph.parallel import parallel
+from tests.rbd.exceptions import RbdBaseException
+from tests.rbd.rbd_utils import initial_rbd_config
+from utility.log import Log
+from utility.utils import run_fio
+
+log = Log(__name__)
+
+
+def image_metadata_feature_disable(rbd, pool_type, **kw):
+    """
+        Disable some existing Image Feature which have Image metadata set on it with IO inprogress.
+    Args:
+        rbd: RBD object
+        pool_type: pool type (ec_pool_config or rep_pool_config)
+        **kw: test data
+    """
+
+    try:
+        pool = kw["config"][pool_type]["pool"]
+        image = kw["config"][pool_type]["image"]
+        snap = kw["config"][pool_type].get("snap", f"{pool}_{image}_snap")
+        clone = kw["config"][pool_type].get("clone", f"{pool}_{image}_clone")
+        snap_name = f"{pool}/{image}@{snap}"
+        feature_name = "deep-flatten"
+
+        client = kw["ceph_cluster"].get_nodes(role="client")[0]
+        run_fio(image_name=image, pool_name=pool, client_node=client)
+        
+        with parallel() as p:
+            p.spawn(rbd.disable_rbd_feature, pool, clone, feature_name)
+            p.spawn(rbd.flatten_clone, pool, clone)
+
+        # Verify the feature is disabled and not present in the image info
+        image_load = rbd.image_info(pool, clone)
+        if feature_name in image_load.get("features", {}):
+            log.error(
+                f"Feature {feature_name} is present in the rbd info of {pool}/{clone}."
+            )
+            return 1
+        else:
+            log.debug(f" Image feature {feature_name} disabled successfully")
+
+        return 0
+
+    except RbdBaseException as error:
+        log.error(error.message)
+        return 1
+
+    finally:
+        rbd.clean_up(pools=[kw["config"][pool_type]["pool"]])
+
+
+def run(**kw):
+    """Perform flatten operations while changing the image feature.
+    This test disables the image feature while flatten operation in progress
+    Args:
+        kw: test data
+    Returns:
+        int: The return value. 0 for success, 1 otherwise
+    Test case covered -
+    CEPH-9862 - Perform flatten operations while changing the image feature.
+    Pre-requisites :
+    1. Cluster must be up and running with capacity to create pool
+    2. We need atleast one client node with ceph-common package,
+       conf and keyring files
+    Test Case Flow:
+    1. Create a pool
+    2. Create an image
+    3. Run Ios on an Image
+    4. Take a snapshot
+    5. Protect a snapshot
+    6. clone a snapshot
+    7. While changing the image features, perform flatten operations
+        Image features should be changed and flatten operation should succeed
+    8. Repeat steps 1 to 7 for ecpool
+    """
+    log.info("Test to disable image feature when flatten operation is performed")
+    rbd_obj = initial_rbd_config(**kw)
+    if rbd_obj:
+        log.info("Executing test on Replication pool")
+        if flatten_image_feature_disable(
+            rbd_obj.get("rbd_reppool"), "rep_pool_config", **kw
+        ):
+            return 1
+        log.info("Executing test on EC pool")
+        if flatten_image_feature_disable(
+            rbd_obj.get("rbd_ecpool"), "ec_pool_config", **kw
+        ):
+            return 1
+    return 0
