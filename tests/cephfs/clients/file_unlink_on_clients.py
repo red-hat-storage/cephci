0475d6be8ffeb6e97ad3d5532c5d2d0cd5722810
Amarnath K <amk@li-132ff74c-24e7-11b2-a85c-eca3000aa1e4.ibm.com>
2023-04-27 00:15:16 +0530

    Adding FS TC 83575042
    Signed-off-by: Amarnath K <amk@li-132ff74c-24e7-11b2-a85c-eca3000aa1e4.ibm.com>

---
+++ tests/cephfs/clients/file_unlink_on_clients.py
@@ -0,0 +1,125 @@
+import random
+import string
+import traceback
+
+from ceph.ceph import CommandFailed
+from tests.cephfs.cephfs_utilsV1 import FsUtils
+from utility.log import Log
+
+log = Log(__name__)
+
+
+def run(ceph_cluster, **kw):
+    """
+    CEPH-83575042 - Test file unlink , stat , open concurrently
+
+    Steps Performed:
+    1. Create FS and mount on all the ways (Fuse, kernel, NFS)
+    2. Create a file in mount and lock it.
+    3. Try to unlink the file from kernel mount from other node
+    4. file should be deleted
+    Args:
+        ceph_cluster:
+        **kw:
+    Exception : FCNTL locking is not working on nfs mounts
+    Returns:
+        0 --> if test PASS
+        1 --> if test FAIL
+
+    """
+    try:
+        log.info(f"MetaData Information {log.metadata} in {__name__}")
+        fs_util = FsUtils(ceph_cluster)
+
+        config = kw.get("config")
+        build = config.get("build", config.get("rhbuild"))
+        clients = ceph_cluster.get_ceph_objects("client")
+        clients[0].upload_file(
+            "tests/cephfs/clients/file_lock_utitlity.py",
+            "/home/cephuser/file_lock_utility.py",
+            sudo=True,
+        )
+        clients[1].upload_file(
+            "tests/cephfs/clients/file_lock_utitlity.py",
+            "/home/cephuser/file_lock_utility.py",
+            sudo=True,
+        )
+        version, rc = clients[0].exec_command(
+            sudo=True, cmd="ceph version --format json"
+        )
+        fs_util.prepare_clients([clients[0]], build)
+        fs_util.auth_list([clients[0], clients[1]])
+        if not build.startswith(("3", "4", "5")):
+            if not fs_util.validate_fs_info(clients[0], "cephfs"):
+                log.error("FS info Validation failed")
+                return 1
+        mounting_dir = "".join(
+            random.choice(string.ascii_lowercase + string.digits)
+            for _ in list(range(10))
+        )
+        fuse_mounting_dir = f"/mnt/cephfs_fuse{mounting_dir}/"
+        fs_util.fuse_mount([clients[0], clients[1]], fuse_mounting_dir)
+
+        kernel_mounting_dir = f"/mnt/cephfs_kernel{mounting_dir}/"
+        mon_node_ips = fs_util.get_mon_node_ips()
+        fs_util.kernel_mount(
+            [clients[0], clients[1]], kernel_mounting_dir, ",".join(mon_node_ips)
+        )
+        rc = unlink_file(
+            clients[0],
+            clients[1],
+            "fuse_mount.txt",
+            fuse_mounting_dir,
+            validate_from=[kernel_mounting_dir],
+        )
+
+        if rc:
+            raise CommandFailed("Unlink of the file is failing when file is locked")
+        rc = unlink_file(
+            clients[0],
+            clients[1],
+            "kernel_mount.txt",
+            kernel_mounting_dir,
+            validate_from=[fuse_mounting_dir],
+        )
+        if rc:
+            raise CommandFailed("Unlink of the file is failing when file is locked")
+
+        return 0
+
+    except Exception as e:
+        log.error(e)
+        log.error(traceback.format_exc())
+        return 1
+    finally:
+        log.info("---clean up---------")
+        fs_util.client_clean_up(
+            "umount", fuse_clients=[clients[0]], mounting_dir=fuse_mounting_dir
+        )
+        fs_util.client_clean_up(
+            "umount",
+            kernel_clients=[clients[0]],
+            mounting_dir=kernel_mounting_dir,
+        )
+
+
+def unlink_file(client1, client2, file_name, lock_from_mount_dir, validate_from=[]):
+    client1.exec_command(sudo=True, cmd=f"touch {lock_from_mount_dir}{file_name}")
+    client1.exec_command(
+        sudo=True,
+        cmd=f"python3 /home/cephuser/file_lock_utility.py {lock_from_mount_dir}{file_name} lock --timeout 120 &",
+        long_running=True,
+    )
+    out, rc = client2.exec_command(
+        sudo=True,
+        cmd=f"unlink {validate_from[0]}{file_name}",
+    )
+    out, rc = client1.exec_command(
+        sudo=True, cmd=f"ls -lrt {lock_from_mount_dir}{file_name}", check_ec=False
+    )
+    log.info(out)
+    log.info(rc)
+    if rc == 0:
+        raise CommandFailed(
+            f"Unlink of {lock_from_mount_dir}{file_name} has not deleted it"
+        )
