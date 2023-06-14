f2bf2e94e38bcd76d15cc63b21975afa83739ef3
vinpapnoi <vpapnoi@redhat.com>
2023-06-14 12:25:44 -0400

    [CephMgr][Tier2] Verify enabling and disabling ceph-mgr plugins
    Signed-off-by: vinpapnoi <vpapnoi@redhat.com>

---
+++ tests/mgr/test_cephmgr_plugins.py
@@ -0,0 +1,38 @@
+from cli.cephadm.cephadm import CephAdm
+from cli.exceptions import OperationFailedError
+
+
+def run(ceph_cluster, **kw):
+    """Verify Ceph Mgr plugins
+    Args:
+        **kw: Key/value pairs of configuration information
+              to be used in the test.
+    """
+    node = ceph_cluster.get_nodes(role="mgr")[0]
+    plugins = [
+        "influx",
+        "k8sevents",
+        "prometheus",
+        "restful",
+        "rook",
+        "stats",
+        "telegraf",
+        "telemetry",
+        "zabbix",
+    ]
+
+    for plugin in plugins:
+        # Enable ceph-mgr plugin
+        if CephAdm(node).ceph.mgr.module(action="enable", module=plugin, force=True):
+            OperationFailedError(f"Failed to enable {plugin} plugin")
+
+        # Verify ceph-mgr plugin being listed under enabled modules
+        out = CephAdm(node).ceph.mgr.module(action="ls")
+        if plugin not in out:
+            OperationFailedError(f"{plugin} plug-in not listed under enabled_modules")
+
+        # Disable ceph-mgr plugin
+        if CephAdm(node).ceph.mgr.module(action="disable", module=plugin):
+            OperationFailedError(f"Failed to disable {plugin} plugin")
+
+    return 0
