1a59992beb6342101fb00be91e2dd24b79131973
sunilkumarn417 <sunnagar@redhat.com>
2023-09-06 17:40:38 +0530

    Added nvme-cli utility
    Signed-off-by: sunilkumarn417 <sunnagar@redhat.com>

---
+++ ceph/nvmeof/nvmeof_gwcli.py
@@ -0,0 +1,180 @@
+"""Ceph-NVMeoF gateway module.
+
+- Configure spdk and start spdk.
+- Configure nvme-of targets using control.cli.
+"""
+from json import loads
+
+from ceph.ceph_admin.common import config_dict_to_string
+from ceph.ceph_admin.orch import Orch
+from utility.log import Log
+
+LOG = Log(__name__)
+
+CEPH_NVMECLI_IMAGE = "quay.io/ceph/nvmeof-cli:0.0.3"
+
+
+def find_client_daemon_id(cluster, pool_name):
+    """Find client daemon Id."""
+    orch = Orch(cluster=cluster, **{})
+    daemons, _ = orch.ps(
+        {"base_cmd_args": {"format": "json"}, "args": {"daemon_type": "nvmeof"}}
+    )
+    for daemon in loads(daemons):
+        if daemon["service_name"] == f"nvmeof.{pool_name}":
+            return f"client.{daemon['daemon_name']}"
+
+
+class NVMeCLI:
+    """NVMeCLI class for managing NVMeoF Gateway entities."""
+
+    def __init__(self, node, port=5500):
+        self.node = node
+        self.port = port
+        self.check()
+
+    def check(self):
+        """Method to pre-check the pre-requisites.
+        - check for podman utility
+        """
+        check_lists = ["dnf install podman -y"]
+        for cmd in check_lists:
+            self.node.exec_command(cmd=cmd, sudo=True)
+
+    def run_control_cli(self, action, **cmd_args):
+        """Run CLI via control daemon."""
+        base_cmd = f"podman run {CEPH_NVMECLI_IMAGE}"
+        base_cmd += (
+            f" --server-address {self.node.ip_address} --server-port {self.port}"
+        )
+        out = self.node.exec_command(
+            sudo=True,
+            cmd=f"{base_cmd} {action} {config_dict_to_string(cmd_args)}",
+        )
+        LOG.debug(out)
+        return out
+
+    def get_subsystems(self):
+        """Get all subsystems."""
+        return self.run_control_cli("get_subsystems")
+
+    def create_block_device(self, name, image, pool, block_size=None):
+        """Create block device using rbd image."""
+        args = {"image": image, "pool": pool, "bdev": name}
+        if block_size:
+            args["block-size"] = block_size
+        return self.run_control_cli("create_bdev", **args)
+
+    def delete_block_device(self, name):
+        """Delete block device."""
+        args = {"bdev": name}
+        return self.run_control_cli("delete_bdev", **args)
+
+    def create_subsystem(self, subnqn, serial_num, max_ns=None):
+        """Create subsystem."""
+        args = {"subnqn": subnqn, "serial": serial_num}
+        if max_ns:
+            args["max-namespaces"] = max_ns
+        return self.run_control_cli("create_subsystem", **args)
+
+    def delete_subsystem(self, subnqn):
+        """Delete subsystem."""
+        return self.run_control_cli("delete_subsystem", **{"subnqn": subnqn})
+
+    def add_namespace(self, subnqn, bdev, nsid=None):
+        """Add namespace under subsystem."""
+        args = {"subnqn": subnqn, "bdev": bdev}
+        if nsid:
+            args["nsid"] = nsid
+        return self.run_control_cli("add_namespace", **args)
+
+    def remove_namespace(self, subnqn, bdev):
+        """Remove namespace under subsystem."""
+        args = {"subnqn": subnqn}
+        _, subsystems = self.get_subsystems()
+        for sub in loads("\n".join(subsystems.split("\n")[1:])):
+            if sub["nqn"] == subnqn:
+                for namespace in sub["namespaces"]:
+                    if namespace["name"] == bdev:
+                        args["nsid"] = namespace["nsid"]
+        return self.run_control_cli("remove_namespace", **args)
+
+    def add_host(self, subnqn, hostnqn):
+        """Add host to subsystem."""
+        args = {"subnqn": subnqn, "host": repr(hostnqn)}
+        return self.run_control_cli("add_host", **args)
+
+    def remove_host(self, subnqn, hostnqn):
+        """Remove host from subsystem."""
+        args = {"subnqn": subnqn, "host": repr(hostnqn)}
+        return self.run_control_cli("remove_host", **args)
+
+    def create_listener(self, subnqn, port, **kwargs):
+        """Create listener under subsystem.
+
+        Args:
+            subnqn: subsystem nqn
+            port: transport channel port
+            kwargs: other attributes
+        """
+        args = {
+            "subnqn": subnqn,
+            "trsvcid": port,
+            "gateway-name": kwargs.get("gateway-name", False),
+            "trtype": kwargs.get("trtype", False),
+            "adrfam": kwargs.get("adrfam", False),
+            "traddr": kwargs.get("traddr", False),
+        }
+        return self.run_control_cli("create_listener", **args)
+
+    def delete_listener(self, subnqn, port, **kwargs):
+        """Delete listener under subsystem.
+
+        Args:
+            subnqn: subsystem nqn
+            port: transport channel port
+            kwargs: other attributes
+        """
+        args = {
+            "subnqn": subnqn,
+            "trsvcid": port,
+            "gateway-name": kwargs.get("gateway-name", False),
+            "trtype": kwargs.get("trtype", False),
+            "adrfam": kwargs.get("adrfam", False),
+            "traddr": kwargs.get("traddr", False),
+        }
+        return self.run_control_cli("delete_listener", **args)
+
+    # # workflows
+    # def configure_subsystems(self, config):
+    #     """Configure Ceph-NVMEoF Subsystems."""
+    #     sub_nqn = config["nqn"]
+    #     max_ns = config.get("max_ns", 32)
+    #     config
+    #     self.create_subsystem(sub_nqn, config["serial"], max_ns)
+    #     self.create_listener(sub_nqn, config["listener_port"])
+    #     self.add_host(sub_nqn, config["allow_host"])
+    #     if config.get("bdevs"):
+    #         name = generate_unique_id(length=4)
+    #         with parallel() as p:
+    #             count = config["bdevs"].get("count", 1)
+    #             size = config["bdevs"].get("size", "1G")
+    #             # Create image
+    #             for num in range(count):
+    #                 p.spawn(rbd.create_image, pool, f"{name}-image{num}", size)
+    #
+    #         with parallel() as p:
+    #             # Create block device in gateway
+    #             for num in range(count):
+    #                 p.spawn(
+    #                     self.create_block_device,
+    #                     f"{name}-bdev{num}",
+    #                     f"{name}-image{num}",
+    #                     pool,
+    #                 )
+    #
+    #         with parallel() as p:
+    #             # Add namespace
+    #             for num in range(count):
+    #                 p.spawn(gw.add_namespace, sub_nqn, f"{name}-bdev{num}")
+    #     pass
