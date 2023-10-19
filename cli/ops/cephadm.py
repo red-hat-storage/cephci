0d40f3b3d4ca7ff3458613a8bd2c7e4c0e19db01
Vaibhav Mahajan <vamahaja@redhat.com>
2023-10-19 17:02:41 +0530

    [LibFix] Refactor cli bootstrap module
    Signed-off-by: Vaibhav Mahajan <vamahaja@redhat.com>

--- cli/ops/cephadm.py
+++ cli/ops/cephadm.py
@@ -1,5 +1,9 @@
+import tempfile
+
 from cli.cephadm.cephadm import CephAdm
-from cli.exceptions import OperationFailedError
+from cli.exceptions import OperationFailedError, ResourceNotFoundError
+from cli.utilities.configs import get_registry_details
+from cli.utilities.utils import get_node_ip
 
 CONTAINER_IMAGE_CONFIG = "mgr/cephadm/container_image_{}"
 CONTAINER_IMAGES = [
@@ -14,6 +18,30 @@ CONTAINER_IMAGES = [
     "loki",
     "nvmeof",
 ]
+
+
+def generate_bootstrap_config(node, config):
+    """Create json with bootsrap config
+
+    Args:
+        node (CephInstallerNode): Ceph installer node
+        config (str): Bootstrap config
+    """
+    # Create temporory file path
+    temp_file = tempfile.NamedTemporaryFile(suffix=".conf")
+
+    # Generate config
+    _config = ""
+    for k in config.keys():
+        _config += f"[{k}]\n"
+        _config += "\n".join(config.get(k).split(" "))
+
+    # Create temporary file and dump data
+    with node.remote_file(sudo=True, file_name=temp_file.name, file_mode="w") as _f:
+        _f.write(f"{_config}\n")
+        _f.flush()
+
+    return temp_file.name
 
 
 def set_container_image_config(node, configs):
@@ -56,21 +84,30 @@ def set_container_image_config(node, configs):
     return True
 
 
-def bootstrap(node, **kw):
+def bootstrap(
+    installer,
+    nodes=None,
+    ibm_build=False,
+    image=None,
+    **kw,
+):
     """Bootstrap cluster
 
     Args:
-        node (CephInstallerNode): Ceph installer node
+        installer (CephInstallerNode): Ceph installer node
+        build_type (str): Build tag
+        nodes (list): List of CephNode objects
+        ibm_build (bool): True in case build is IBM
+        image (str): Ceph container image
         kw (dict): Key/Value pairs to be provided to bootstrap
             Supported keys:
                 fsid (str): Cluster FSID
-                image (str): Container image
                 yes_i_know (bool): Flag to set option `yes-i-know`
                 registry-url (str): URL for custom registry
                 registry-username (str): Username for custom registry
                 registry-password (str): Password for custom registry
                 registry-json (str): json file with custom registry login info
-                mon-ip (str): Mon IP address
+                mon-ip (str): Mon node id
                 config (str): Ceph conf file to incorporate
                 skip-dashboard (str): Do not enable the Ceph Dashboard
                 initial-dashboard-user (str): Initial user for the dashboard
@@ -81,14 +118,26 @@ def bootstrap(node, **kw):
                 apply-spec (str): Apply cluster spec after bootstrap
                 cluster-network (str): Subnet to use for cluster replication, recovery and heartbeats
     """
-    # Get ceph image
-    image = kw.pop("image") if kw.get("image") else None
+    # Check for mon IP
+    if kw.get("mon-ip"):
+        if not nodes:
+            raise ResourceNotFoundError("Nodes are required to get mon id")
+
+        kw["mon-ip"] = get_node_ip(nodes, kw.pop("mon-ip"))
+
+    # Check for config
+    if kw.get("config"):
+        kw["config"] = generate_bootstrap_config(installer, kw.pop("config"))
+
+    # Check for registry details
+    if not kw.get("registry-url") and ibm_build:
+        kw.update(get_registry_details(ibm_build))
 
     # Get yes-i-know tag
     yes_i_know = kw.pop("yes-i-know") if kw.get("yes-i-know") else None
 
     # Bootstrap ceph cluster
-    if CephAdm(node).bootstrap(image=image, yes_i_know=yes_i_know, **kw):
+    if CephAdm(installer).bootstrap(image=image, yes_i_know=yes_i_know, **kw):
         raise OperationFailedError("Failed to bootstrap cluster")
 
     return True
