bd37057dcb143dd1d06d399e835551fd6739736f
Amarnath K <amk@li-132ff74c-24e7-11b2-a85c-eca3000aa1e4.ibm.com>
2023-11-28 16:42:00 +0530

    Adding FS TC 83575340
    Signed-off-by: Amarnath K <amk@li-132ff74c-24e7-11b2-a85c-eca3000aa1e4.ibm.com>

---
+++ tests/cephfs/cephfs_mirroring/test_cephfs_mirror_ha_conversion.py
@@ -0,0 +1,303 @@
+import random
+import string
+import traceback
+
+from ceph.ceph import CommandFailed
+from tests.cephfs.cephfs_mirroring.cephfs_mirroring_utils import CephfsMirroringUtils
+from tests.cephfs.cephfs_utilsV1 import FsUtils
+from utility.log import Log
+
+log = Log(__name__)
+
+
+def run(ceph_cluster, **kw):
+    """
+    CEPH-83575340 - Convert a standalone CephFS Mirroring service to HA and
+    validate all the existing paths part of mirroring are intact and continues to sync post convertion to HA service
+    It has pre-requisites,
+    performs various operations, and cleans up the system after the tests.
+
+    Args:
+        ceph_cluster: The Ceph cluster to perform the mirroring tests on.
+        **kw: Additional keyword arguments.
+
+    Returns:
+        int: 0 if the test is successful, 1 if there's an error.
+
+    Raises:
+        Exception: Any unexpected exceptions that might occur during the test.
+    """
+
+    try:
+        config = kw.get("config")
+        ceph_cluster_dict = kw.get("ceph_cluster_dict")
+        fs_util_ceph1 = FsUtils(ceph_cluster_dict.get("ceph1"))
+        fs_util_ceph2 = FsUtils(ceph_cluster_dict.get("ceph2"))
+        fs_mirroring_utils = CephfsMirroringUtils(
+            ceph_cluster_dict.get("ceph1"), ceph_cluster_dict.get("ceph2")
+        )
+        build = config.get("build", config.get("rhbuild"))
+        source_clients = ceph_cluster_dict.get("ceph1").get_ceph_objects("client")
+        target_clients = ceph_cluster_dict.get("ceph2").get_ceph_objects("client")
+        cephfs_mirror_node = ceph_cluster_dict.get("ceph1").get_ceph_objects(
+            "cephfs-mirror"
+        )
+
+        log.info("checking Pre-requisites")
+        if not source_clients or not target_clients:
+            log.info(
+                "This test requires a minimum of 1 client node on both ceph1 and ceph2."
+            )
+            return 1
+        log.info("Preparing Clients...")
+        fs_util_ceph1.prepare_clients(source_clients, build)
+        fs_util_ceph2.prepare_clients(target_clients, build)
+        fs_util_ceph1.auth_list(source_clients)
+        fs_util_ceph2.auth_list(target_clients)
+        source_fs = "cephfs"
+        target_fs = "cephfs_rem"
+        target_user = "mirror_remote_ha"
+        target_site_name = "remote_site_ha"
+        fs_details = fs_util_ceph2.get_fs_info(target_clients[0], target_fs)
+        if not fs_details:
+            fs_util_ceph2.create_fs(client=target_clients[0], vol_name=target_fs)
+            fs_util_ceph2.wait_for_mds_process(target_clients[0], target_fs)
+        log.info("Deploy CephFS Mirroring Configuration")
+        fs_mirroring_utils.deploy_cephfs_mirroring(
+            source_fs,
+            source_clients[0],
+            target_fs,
+            target_clients[0],
+            target_user,
+            target_site_name,
+        )
+
+        log.info("Create Subvolumes for adding Data")
+        log.info("Scenario 1 : ")
+        subvolumegroup_list = [
+            {"vol_name": source_fs, "group_name": "subvolgroup_1"},
+        ]
+        for subvolumegroup in subvolumegroup_list:
+            fs_util_ceph1.create_subvolumegroup(source_clients[0], **subvolumegroup)
+
+        subvolume_list = [
+            {
+                "vol_name": source_fs,
+                "subvol_name": "subvol_1",
+                "group_name": "subvolgroup_1",
+                "size": "5368709120",
+            },
+            {
+                "vol_name": source_fs,
+                "subvol_name": "subvol_2",
+                "group_name": "subvolgroup_1",
+                "size": "5368709120",
+            },
+        ]
+        for subvolume in subvolume_list:
+            fs_util_ceph1.create_subvolume(source_clients[0], **subvolume)
+
+        mounting_dir = "".join(
+            random.choice(string.ascii_lowercase + string.digits)
+            for _ in list(range(10))
+        )
+        log.info("Mount 1 subvolume on kernel and 1 subvloume on Fuse → Client1")
+        kernel_mounting_dir_1 = f"/mnt/cephfs_kernel{mounting_dir}_1/"
+        mon_node_ips = fs_util_ceph1.get_mon_node_ips()
+        log.info("Get the path of subvolume1 on  filesystem")
+        subvol_path1, rc = source_clients[0].exec_command(
+            sudo=True,
+            cmd=f"ceph fs subvolume getpath {source_fs} subvol_1 subvolgroup_1",
+        )
+        index = subvol_path1.find("subvol_1/")
+        if index != -1:
+            subvol1_path = subvol_path1[: index + len("subvol_1/")]
+        else:
+            subvol1_path = subvol_path1
+        log.info(subvol1_path)
+        #
+        fs_util_ceph1.kernel_mount(
+            [source_clients[0]],
+            kernel_mounting_dir_1,
+            ",".join(mon_node_ips),
+        )
+        # log.info("Get the path of subvolume2 on filesystem")
+        fuse_mounting_dir_1 = f"/mnt/cephfs_fuse{mounting_dir}_1/"
+        subvol_path2, rc = source_clients[0].exec_command(
+            sudo=True,
+            cmd=f"ceph fs subvolume getpath {source_fs} subvol_2 subvolgroup_1",
+        )
+        index = subvol_path2.find("subvol_2/")
+        if index != -1:
+            subvol2_path = subvol_path2[: index + len("subvol_2/")]
+        else:
+            subvol2_path = subvol_path2
+        log.info(subvol2_path)
+        fs_util_ceph1.fuse_mount(
+            [source_clients[0]],
+            fuse_mounting_dir_1,
+        )
+        #
+        log.info("Add subvolumes for mirroring to remote location")
+        fs_mirroring_utils.add_path_for_mirroring(
+            source_clients[0], source_fs, subvol1_path
+        )
+        fs_mirroring_utils.add_path_for_mirroring(
+            source_clients[0], source_fs, subvol2_path
+        )
+        fs_mirroring_utils.add_files_and_validate(
+            source_clients,
+            kernel_mounting_dir_1,
+            subvol1_path,
+            fuse_mounting_dir_1,
+            subvol2_path,
+            cephfs_mirror_node,
+            source_fs,
+            "single_service_8",
+            snap_count=2,
+        )
+        md5sum = "md5sum_script.sh"
+        target_clients[0].upload_file(
+            sudo=True,
+            src="tests/cephfs/cephfs_mirror_upgrade/md5sum_script.sh",
+            dst=f"/root/{md5sum}",
+        )
+        source_clients[0].upload_file(
+            sudo=True,
+            src="tests/cephfs/cephfs_mirror_upgrade/md5sum_script.sh",
+            dst=f"/root/{md5sum}",
+        )
+        out, rc = fs_mirroring_utils.list_and_verify_remote_snapshots_and_data_checksum(
+            target_clients[0],
+            "client.admin",
+            subvol2_path,
+            source_clients[0],
+            fuse_mounting_dir_1,
+            target_fs,
+        )
+        if not out:
+            raise CommandFailed(f"{rc}")
+        out, rc = fs_mirroring_utils.list_and_verify_remote_snapshots_and_data_checksum(
+            target_clients[0],
+            "client.admin",
+            subvol1_path,
+            source_clients[0],
+            kernel_mounting_dir_1,
+            target_fs,
+        )
+        if not out:
+            raise CommandFailed(f"{rc}")
+        mds_nodes = ceph_cluster_dict.get("ceph1").get_ceph_objects("mds")
+        mds_names_hostname = []
+        for mds in mds_nodes:
+            mds_names_hostname.append(mds.node.hostname)
+        if len(mds_names_hostname) > 2:
+            hosts_list1 = mds_names_hostname[1:3]
+        else:
+            hosts_list1 = mds_names_hostname
+
+        mds_hosts_1 = " ".join(hosts_list1)
+        log.info(f"MDS host list 1 {mds_hosts_1}")
+        source_clients[0].exec_command(
+            sudo=True,
+            cmd=f"ceph orch apply cephfs-mirror --placement='2 {mds_hosts_1}'",
+        )
+        fs_util_ceph1.validate_services(source_clients[0], "cephfs-mirror")
+        peer_uuid = fs_mirroring_utils.get_peer_uuid_by_name(
+            source_clients[0], source_fs
+        )
+        if len(mds_nodes) > 2:
+            cephfs_mirror_node = mds_nodes[1:3]
+        else:
+            cephfs_mirror_node = mds_nodes
+        fs_mirroring_utils.add_files_and_validate(
+            source_clients,
+            kernel_mounting_dir_1,
+            subvol1_path,
+            fuse_mounting_dir_1,
+            subvol2_path,
+            cephfs_mirror_node,
+            source_fs,
+            "ha_service_22",
+            snap_count=1,
+        )
+
+        out, rc = fs_mirroring_utils.list_and_verify_remote_snapshots_and_data_checksum(
+            target_clients[0],
+            "client.admin",
+            subvol2_path,
+            source_clients[0],
+            fuse_mounting_dir_1,
+            target_fs,
+        )
+        if not out:
+            raise CommandFailed(f"{rc}")
+        out, rc = fs_mirroring_utils.list_and_verify_remote_snapshots_and_data_checksum(
+            target_clients[0],
+            "client.admin",
+            subvol1_path,
+            source_clients[0],
+            kernel_mounting_dir_1,
+            target_fs,
+        )
+        if not out:
+            raise CommandFailed(f"{rc}")
+        return 0
+    except Exception as e:
+        log.error(e)
+        log.error(traceback.format_exc())
+        return 1
+    finally:
+        log.info("Clean up the system")
+        log.info("Delete the snapshots")
+        snapshots_to_delete = [
+            f"{kernel_mounting_dir_1}{subvol1_path}.snap/snap_k1_single_service*",
+            f"{kernel_mounting_dir_1}{subvol1_path}.snap/snap_k1_ha_service_*",
+            f"{fuse_mounting_dir_1}{subvol2_path}.snap/snap_f1_single_service*",
+            f"{fuse_mounting_dir_1}{subvol2_path}.snap/snap_f1_ha_service_*",
+        ]
+        for snapshot_path in snapshots_to_delete:
+            source_clients[0].exec_command(
+                sudo=True, cmd=f"rmdir {snapshot_path}", check_ec=False
+            )
+
+        log.info("Unmount the paths")
+        paths_to_unmount = [kernel_mounting_dir_1, fuse_mounting_dir_1]
+        for path in paths_to_unmount:
+            source_clients[0].exec_command(sudo=True, cmd=f"umount -l {path}")
+
+        log.info("Remove paths used for mirroring")
+        paths_to_remove = [subvol1_path, subvol2_path]
+        for path in paths_to_remove:
+            fs_mirroring_utils.remove_path_from_mirroring(
+                source_clients[0], source_fs, path
+            )
+
+        log.info("Destroy CephFS Mirroring setup.")
+        fs_mirroring_utils.destroy_cephfs_mirroring(
+            source_fs,
+            source_clients[0],
+            target_fs,
+            target_clients[0],
+            target_user,
+            peer_uuid,
+        )
+
+        log.info("Remove Subvolumes")
+        for subvolume in subvolume_list:
+            fs_util_ceph1.remove_subvolume(
+                source_clients[0],
+                **subvolume,
+            )
+
+        log.info("Remove Subvolume Group")
+        for subvolumegroup in subvolumegroup_list:
+            fs_util_ceph1.remove_subvolumegroup(source_clients[0], **subvolumegroup)
+
+        log.info("Delete the mounted paths")
+        source_clients[0].exec_command(sudo=True, cmd=f"rm -rf {kernel_mounting_dir_1}")
+        source_clients[0].exec_command(sudo=True, cmd=f"rm -rf {fuse_mounting_dir_1}")
+        source_clients[0].exec_command(
+            sudo=True,
+            cmd="ceph orch apply cephfs-mirror --placement='label:cephfs-mirror'",
+        )
