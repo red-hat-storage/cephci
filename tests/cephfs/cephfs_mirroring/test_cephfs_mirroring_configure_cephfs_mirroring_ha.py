ec59b70f4fa835875590ca6b2c17ea456ba4ae5d
Amarnath K <amk@li-132ff74c-24e7-11b2-a85c-eca3000aa1e4.ibm.com>
2023-12-06 23:40:42 +0530

    Modifying mirror ha adhering to mirror_utils
    Signed-off-by: Amarnath K <amk@li-132ff74c-24e7-11b2-a85c-eca3000aa1e4.ibm.com>

--- tests/cephfs/cephfs_mirroring/test_cephfs_mirroring_configure_cephfs_mirroring_ha.py
+++ tests/cephfs/cephfs_mirroring/test_cephfs_mirroring_configure_cephfs_mirroring_ha.py
@@ -221,7 +221,7 @@ def run(ceph_cluster, **kw):
                 cephfs_mirror_node[0],
                 source_fs,
                 fsid,
-                asok_file1,
+                asok_file1[0],
                 filesystem_id,
                 peer_uuid,
             )
@@ -237,7 +237,7 @@ def run(ceph_cluster, **kw):
                 cephfs_mirror_node[1],
                 source_fs,
                 fsid,
-                asok_file2,
+                asok_file2[0],
                 filesystem_id,
                 peer_uuid,
             )
