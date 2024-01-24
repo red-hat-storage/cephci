6178a2d967f29d892bb987452d845c8456272205
Mohit <mobisht@redhat.com>
2024-01-24 02:25:44 -0500

    [NFS-Ganesha][Reef][Upgrade]update upgrade suite
    Signed-off-by: Mohit <mobisht@redhat.com>

--- tests/nfs/nfs_verify_multiple_parallel_io_and_lookups.py
+++ tests/nfs/nfs_verify_multiple_parallel_io_and_lookups.py
@@ -22,6 +22,9 @@ def run(ceph_cluster, **kw):
     port = config.get("port", "2049")
     version = config.get("nfs_version", "4.0")
     no_clients = int(config.get("clients", "2"))
+    setup = config.get("setup", True)
+    io = config.get("io", True)
+    cleanup = config.get("io", True)
 
     # If the setup doesn't have required number of clients, exit.
     if no_clients > len(clients):
@@ -37,59 +40,62 @@ def run(ceph_cluster, **kw):
     nfs_server_name = nfs_node.hostname
 
     try:
-        # Setup nfs cluster
-        setup_nfs_cluster(
-            clients,
-            nfs_server_name,
-            port,
-            version,
-            nfs_name,
-            nfs_mount,
-            fs_name,
-            nfs_export,
-            fs,
-            ceph_cluster=ceph_cluster,
-        )
-
-        # Linux untar on client 1
-        th1 = Thread(
-            target=linux_untar,
-            args=(
-                clients[0],
+        if setup:
+            # Setup nfs cluster
+            setup_nfs_cluster(
+                clients,
+                nfs_server_name,
+                port,
+                version,
+                nfs_name,
                 nfs_mount,
-            ),
-        )
+                fs_name,
+                nfs_export,
+                fs,
+                ceph_cluster=ceph_cluster,
+            )
 
-        # Do file creation from client 2
-        th2 = Thread(
-            target=create_files,
-            args=(clients[1], nfs_mount, 100),
-        )
+        if io:
+            # Linux untar on client 1
+            th1 = Thread(
+                target=linux_untar,
+                args=(
+                    clients[0],
+                    nfs_mount,
+                ),
+            )
 
-        th1.start()
-        th2.start()
+            # Do file creation from client 2
+            th2 = Thread(
+                target=create_files,
+                args=(clients[1], nfs_mount, 100),
+            )
 
-        # While the IO's are in progress, do the look ups in parallel
-        while th1.is_alive() or th2.is_alive():
-            # Test 1: Perform Linux untar from 1 client and do readir operation from other client (ls -lart)
-            cmd = f"ls -lart {nfs_mount}"
-            clients[1].exec_command(cmd=cmd, sudo=True)
+            th1.start()
+            th2.start()
 
-            # Test 2: Perform Linux untar from 1 client and do readir operation from other client (du -sh)
-            cmd = f"du -sh {nfs_mount}"
-            clients[2].exec_command(cmd=cmd, sudo=True)
+            # While the IO's are in progress, do the look ups in parallel
+            while th1.is_alive() or th2.is_alive():
+                # Test 1: Perform Linux untar from 1 client and do readir operation from other client (ls -lart)
+                cmd = f"ls -lart {nfs_mount}"
+                clients[1].exec_command(cmd=cmd, sudo=True)
 
-        th1.join()
-        th2.join()
+                # Test 2: Perform Linux untar from 1 client and do readir operation from other client (du -sh)
+                cmd = f"du -sh {nfs_mount}"
+                clients[2].exec_command(cmd=cmd, sudo=True)
+
+            th1.join()
+            th2.join()
 
     except Exception as e:
         log.error(f"Failed to validate multiple ios and lookups : {e}")
         return 1
     finally:
-        log.info("Cleaning up")
-        import time
+        if cleanup:
+            log.info("Cleaning up")
+            import time
 
-        time.sleep(20)
-        cleanup_cluster(clients, nfs_mount, nfs_name, nfs_export)
-        log.info("Cleaning up successful")
+            time.sleep(20)
+            cleanup_cluster(clients, nfs_mount, nfs_name, nfs_export)
+            log.info("Cleaning up successful")
     return 0
