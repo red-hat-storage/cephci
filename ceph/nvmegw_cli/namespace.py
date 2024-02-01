8f7104f9b8890633eef6a9a712044b54b043b334
Chebrolu <hchebrol@redhat.com>
2024-02-01 11:39:04 +0530

    automate testcases for qos of namespace
    Signed-off-by: Chebrolu <hchebrol@redhat.com>

--- ceph/nvmegw_cli/namespace.py
+++ ceph/nvmegw_cli/namespace.py
@@ -1,3 +1,5 @@
+import re
+
 from ceph.nvmegw_cli import NVMeGWCLI
 
 
@@ -7,16 +9,29 @@ class Namespace(NVMeGWCLI):
         self.name = "namespace"
 
     def set_qos(self, **kwargs):
+        """Set QoS for namespace."""
+        subsystem = kwargs.get("args", {}).get("subsystem")
+        _, namespaces = self.list(args={"subsystem": subsystem})
+
+        pattern = r"\│\s*(\d+)\s*│"
+        nsid = [int(match) for match in re.findall(pattern, namespaces)]
+
+        kwargs.setdefault("args", {}).update({"nsid": nsid[0]})
+
         return self.run_nvme_cli("set_qos", **kwargs)
 
     def add(self, **kwargs):
+        """Adds namespace for subsystem."""
         return self.run_nvme_cli("add", **kwargs)
 
     def delete(self, **kwargs):
+        """Deletes  namespace."""
         return self.run_nvme_cli("del", **kwargs)
 
     def list(self, **kwargs):
+        """Lists namespaces under subsystem."""
         return self.run_nvme_cli("list", **kwargs)
 
     def resize(self, **kwargs):
+        """Resize namespaces under subsystem."""
         return self.run_nvme_cli("resize", **kwargs)
