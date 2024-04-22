f9ee60ccae8bf4795633620aa4b585d2f728458d
Sunil Kumar Nagaraju <sunnagar@redhat.com>
2024-04-22 20:54:21 +0530

    server addr,port custom support and new log-level attr
    Signed-off-by: Sunil Kumar Nagaraju <sunnagar@redhat.com>

--- ceph/nvmegw_cli/log_level.py
+++ ceph/nvmegw_cli/log_level.py
@@ -4,7 +4,7 @@ from ceph.nvmegw_cli.execute import ExecuteCommandMixin
 class LogLevel(ExecuteCommandMixin):
     def __init__(self, node, port) -> None:
         super().__init__(node, port)
-        self.name = "log_level"
+        self.name = "spdk_log_level"
 
     def get(self, **kwargs):
         return self.run_nvme_cli("get", **kwargs)
