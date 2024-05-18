ef3522c329ba4ec6b41c09e3fe301490f64cbb56
julpark <yonhyun@gmail.com>
2024-05-17 19:39:12 -0700

    MDS metrics functional automation
    Signed-off-by: julpark <yonhyun@gmail.com>

--- tests/cephfs/cephfs_utilsV1.py
+++ tests/cephfs/cephfs_utilsV1.py
@@ -3525,22 +3525,23 @@ os.system('sudo systemctl start  network')
             mounted_dir (str): where the client is mounted.
 
         Returns:
-            dic: The value of the metric.
+            str: The value of the metric.
         """
         ranked_mds, _ = client.exec_command(
             sudo=True,
             cmd=f"ceph fs status -f json | jq '.mdsmap[] | select(.rank == {rank}) | .name'",
         )
         ranked_mds = ranked_mds.replace('"', "").replace("\n", "")
-        client_id_cmd = f"ceph tell mds.{ranked_mds} session ls | jq '.[] | select(.client_metadata.mount_point != null " \
-                        f"and (.client_metadata.mount_point | contains(\"{mounted_dir}\"))) | .id'"
+        client_id_cmd = f"ceph tell mds.{ranked_mds} session ls | jq '.[] | select(.client_metadata.mount_point" \
+                        f" != null and (.client_metadata.mount_point | contains(\"{mounted_dir}\"))) | .id'"
         client_id, _ = client.exec_command(sudo=True, cmd=client_id_cmd)
         client_id = client_id.replace('"', "").replace("\n", "")
         log.info(f"Client ID : {client_id} for Mounted Directory : {mounted_dir}")
         cmd = f""" ceph tell mds.{ranked_mds} counter dump 2>/dev/null | \
-            jq -r '. | to_entries | map(select(.key | match("mds_client_metrics"))) | \
-            .[].value[] | select(.labels.client != null and (.labels.client | contains("{client_id}")) and (.labels.rank == "0"))'
-            """
+              jq -r '. | to_entries | map(select(.key | match("mds_client_metrics"))) | \
+              .[].value[] | select(.labels.client != null and (.labels.client | contains("{client_id}")) and
+               (.labels.rank == "0"))'
+              """
         metrics_out, _ = client.exec_command(sudo=True, cmd=cmd)
         log.info(
             f"Metrics for MDS : {ranked_mds} Mounted Directory: {mounted_dir} and Client : {client_id} is {metrics_out}"
