c88cf080caed660dde8473d51c3fe5d906e1046c
Aditya Ramteke <aramteke@redhat.com>
2024-07-05 12:17:26 +0530

    [Test] Add test to validate custom config
    Redeploy Grafana and MON services with custom config
    Validate Grafana and MON containers are update with custom config
    
    Signed-off-by: Aditya Ramteke <aramteke@redhat.com>

--- tests/cephadm/test_cephadm_custom_config_support.py
+++ tests/cephadm/test_cephadm_custom_config_support.py
@@ -1,59 +1,93 @@
+from json import loads
+
+from ceph.utils import get_node_by_id
 from ceph.waiter import WaitUntil
 from cli.cephadm.cephadm import CephAdm
-from cli.exceptions import CephadmOpsExecutionError
-from cli.ops.cephadm import generate_bootstrap_config
+from cli.exceptions import CephadmOpsExecutionError, OperationFailedError
 from cli.utilities.containers import Container
+from cli.utilities.utils import create_yaml_config
+from utility.log import Log
+
+log = Log(__name__)
 
 
-def check_custom_file(node, config):
-    """Check custom file updated to conatiner"""
-    for spec in config.get("spec"):
-        service = spec.get("service_name")
-        container = Container(node).ps(
-            filter={"name": service}, format='"{{.ID}} {{.IMAGE}}"'
-        )[0]
-        mount_path = spec.get("custom_configs")[1].get("mount_path")
-        timeout, interval = 300, 6
-        for w in WaitUntil(timeout=timeout, interval=interval):
-            result = Container(node).exec(
-                container=container,
-                interactive=True,
-                tty=True,
-                cmds=f"cat {mount_path}",
-            )[0]
-            if result == spec.get("custom_configs")[1].get("content"):
-                break
-        if w.expired:
-            raise CephadmOpsExecutionError(
-                f"{service} container file content not updated"
-            )
+def _verify_service_status(host, service_type):
+    """Verify service status is running"""
+    has_status = False
+    for w in WaitUntil():
+        CephAdm(host).ceph.orch.ps(refresh=True)
+        conf = {"daemon_type": service_type, "format": "json-pretty"}
+        service_info = loads(CephAdm(host).ceph.orch.ps(**conf))
+        status_desc = [c.get("status_desc") for c in service_info][0]
+        if status_desc == "starting":
+            has_status = True
+            log.info(f"{service_type} service is in starting")
+        if has_status and status_desc == "running":
+            log.info(f"All {service_type} services are up and running")
+            return True
+    if w.expired:
+        raise OperationFailedError(
+            f"{service_type} service failed; expected status: running, found: {status_desc}"
+        )
 
 
-def run(ceph_cluster, **kwargs):
+def run(ceph_cluster, **kw):
     """Verify cephadm custom config file support"""
-    # Get config specs
-    config = kwargs.get("config")
 
-    # Get cluster installer node
+    # Get configs
+    config = kw.get("config")
+
+    # Get the installer node
     installer = ceph_cluster.get_nodes(role="installer")[0]
 
-    # Generate custom config file
-    config_file = generate_bootstrap_config(installer, config)
+    # Get the config spec
+    spec = config.get("spec", {})
+    service_type = spec.get("service_type", {})
+    custom_configs = spec.get("custom_configs", {})[0]
+    mount_path = custom_configs.get("mount_path", {})
+    content = custom_configs.get("content", {})
 
-    # Mount custom config file
-    conf = {"pos_args": []}
-    out = CephAdm(installer, config_file).ceph.orch.apply(input=config_file, **conf)
+    # Generate a custom config yaml file out of spec
+    file = create_yaml_config(installer, spec)
+
+    # Mount and apply custom config file
+    c = {"pos_args": [], "input": file}
+    out = CephAdm(nodes=installer, mount=file).ceph.orch.apply(**c)
     if "Scheduled" not in out:
-        raise CephadmOpsExecutionError("Fail to mount custom config file")
+        raise OperationFailedError(f"Fail to apply {file} file")
 
-    # Redeploy services
-    for spec in config.get("spec"):
-        service = spec.get("service_name")
-        out = CephAdm(installer).ceph.orch.redeploy(service)
-        if "Scheduled" not in out:
-            raise CephadmOpsExecutionError(f"Fail to redeploy {service}")
+    # Redeploy daemon
+    out = CephAdm(nodes=installer).ceph.orch.redeploy(service_type)
+    if "Scheduled" not in out:
+        raise OperationFailedError(f"Fail to redeploy {service_type} daemon")
+
+    # Verify daemon are running
+    _verify_service_status(installer, service_type)
+
+    # Get container ID
+    CephAdm(installer).ceph.orch.ps(refresh=True)
+    conf = {"daemon_type": service_type, "format": "json-pretty"}
+    service_info = loads(CephAdm(nodes=installer).ceph.orch.ps(**conf))
+    container_id = [c.get("container_id") for c in service_info][0]
+    hostname = [c.get("hostname") for c in service_info][0]
+
+    # Convert hostname to ceph object
+    node = get_node_by_id(ceph_cluster, hostname)
 
     # Validate custom file mounted in containers
-    check_custom_file(installer, config)
+    timeout, interval = 60, 6
+    for w in WaitUntil(timeout=timeout, interval=interval):
+        result = Container(node).exec(
+            container=str(container_id),
+            interactive=True,
+            tty=True,
+            cmds=f"cat {mount_path}",
+        )[0]
+        if result == content:
+            break
+    if w.expired:
+        raise CephadmOpsExecutionError(
+            f"{service_type} container file content not updated"
+        )
 
     return 0
