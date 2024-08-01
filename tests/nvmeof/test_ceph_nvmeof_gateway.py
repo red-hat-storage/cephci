9d72ac92152312c0bbec852098af3ab242bcc0d0
rahullepakshi <rlepaksh@redhat.com>
2024-08-01 19:55:56 +0530

    modify sanity with hostnqn addition for host add
    Signed-off-by: rahullepakshi <rlepaksh@redhat.com>

--- tests/nvmeof/test_ceph_nvmeof_gateway.py
+++ tests/nvmeof/test_ceph_nvmeof_gateway.py
@@ -20,20 +20,30 @@ from utility.utils import generate_unique_id, run_fio
 LOG = Log(__name__)
 
 
-def configure_subsystems(rbd, pool, nvmegwcli, config):
+def configure_subsystems(ceph_cluster, rbd, pool, nvmegwcli, config):
     """Configure Ceph-NVMEoF Subsystems."""
     sub_args = {"subsystem": config["nqn"]}
     nvmegwcli.subsystem.add(
         **{"args": {**sub_args, **{"max-namespaces": config.get("max_ns", 32)}}}
     )
-
     listener_cfg = {
         "host-name": nvmegwcli.fetch_gateway_hostname(),
         "traddr": nvmegwcli.node.ip_address,
         "trsvcid": config["listener_port"],
     }
     nvmegwcli.listener.add(**{"args": {**listener_cfg, **sub_args}})
-    nvmegwcli.host.add(**{"args": {**sub_args, **{"host": repr(config["allow_host"])}}})
+    if config.get("allow_host"):
+        nvmegwcli.host.add(
+            **{"args": {**sub_args, **{"host": repr(config["allow_host"])}}}
+        )
+
+    if config.get("hosts"):
+        for host in config["hosts"]:
+            initiator_node = get_node_by_id(ceph_cluster, host)
+            initiator = Initiator(initiator_node)
+            host_nqn = initiator.nqn()
+            nvmegwcli.host.add(**{"args": {**sub_args, **{"host": host_nqn}}})
+
     if config.get("bdevs"):
         name = generate_unique_id(length=4)
         with parallel() as p:
@@ -292,7 +302,12 @@ def run(ceph_cluster: Ceph, **kwargs) -> int:
             with parallel() as p:
                 for subsys_args in config["subsystems"]:
                     p.spawn(
-                        configure_subsystems, rbd_obj, rbd_pool, nvmegwcli, subsys_args
+                        configure_subsystems,
+                        ceph_cluster,
+                        rbd_obj,
+                        rbd_pool,
+                        nvmegwcli,
+                        subsys_args,
                     )
 
         if config.get("initiators"):
