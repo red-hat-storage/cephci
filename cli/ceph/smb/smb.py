b14dd2d0f9f08bb3498d260ab9eae3456d58d744
Mohit Bisht <mobisht@redhat.com>
2024-08-08 08:59:12 -0400

    [Squid][SMB][Automation]Add scenarios
    Signed-off-by: Mohit Bisht <mobisht@redhat.com>

--- cli/ceph/smb/smb.py
+++ cli/ceph/smb/smb.py
@@ -1,4 +1,5 @@
 from cli import Cli
+from cli.utilities.utils import build_cmd_from_args
 
 from .apply import Apply
 from .cluster import Cluster
@@ -14,3 +15,19 @@ class Smb(Cli):
         self.cluster = Cluster(nodes, self.base_cmd)
         self.share = Share(nodes, self.base_cmd)
         self.apply = Apply(nodes, self.base_cmd)
+
+    def show(self, resource_names, **kw):
+        """
+        Show smb resources
+        Args:
+            resource_names(str): smb resource name
+            format (str): the type to be formatted(yaml or json)
+        """
+        if resource_names:
+            cmd = f"{self.base_cmd} show {resource_names} {build_cmd_from_args(**kw)}"
+        else:
+            cmd = f"{self.base_cmd} show {build_cmd_from_args(**kw)}"
+        out = self.execute(sudo=True, cmd=cmd)
+        if isinstance(out, tuple):
+            return out[0].strip()
+        return out
