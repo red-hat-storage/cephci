c0eb197db7452833ec797f349afaea500122e1f2
Pragadeeswaran Sathyanarayanan <pragadeeswaran.sathyanarayanan@ibm.com>
2024-11-19 20:40:16 +0530

    Decoupling from centralized log class
    The current implementation has multiple limitations due to the
    customization of logger. Some the limitations are
    
    - mangled output when executing in parallel making debugging hard
    - improper module heirachy being applied
    - incorrect logging names.
    
    This commit moves to use the native approach while still retaining
    some of the enhancments required by the framework.
    
    With this change, each test module has full control of the logging
    mechanism. This would allow them to create additional handlers
    without impacting other modules.
    
    Signed-off-by: Pragadeeswaran Sathyanarayanan <pragadeeswaran.sathyanarayanan@ibm.com>

--- utility/log.py
+++ utility/log.py
@@ -1,259 +1,104 @@
 # -*- coding: utf-8 -*-
-"""
-Implements CephCI logging interface.
-
-In this module, we implement a singleton LOG object that can be used by all components
-of CephCI. It supports the below logging methods
-
-    - error
-    - warning
-    - info
-    - debug
-
-Along with the above, it provides support for pushing events to
-
-    - local file
-    - logstash server
-
-Initial Log format will be 'datetime - level - message'
-later updating log format with 'datetime - level -filename:line_number - message'
-"""
-import inspect
+"""Implements CephCI logging related functions."""
+import glob
 import logging
-import logging.handlers
 import os
-from copy import deepcopy
-from typing import Any, Dict
-
-from .config import TestMetaData
-
-LOG_FORMAT = "%(asctime)s (%(name)s) [%(levelname)s] - %(message)s"
-magna_server = "http://magna002.ceph.redhat.com"
-magna_url = f"{magna_server}/cephci-jenkins/"
+import random
+import string
 
 
-class LoggerInitializationException:
-    pass
+FILE_SERVER = "http://magna002.ceph.redhat.com"
+LOG_URL = f"{FILE_SERVER}/cephci-jenkins/"
+LOG_FORMAT = (
+    "%(asctime)s - %(name)s - %(pathname)s:%(lineno)d - "
+    "[%(levelname)s] - %(message)s"
+)
 
 
-class Log:
-    """CephCI Logger object to help streamline logging."""
+def get_log_name(name) -> str:
+    """Return a logger name.
 
-    def __init__(self, name=None) -> None:
-        """Initializes the logging mechanism based on the inputs provided."""
-        self._logger = logging.getLogger("cephci")
-        logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
+    This method returns the given name if there are no file handlers attached
+    to the provided logger. Otherwise it will return the name with a random 8
+    character prefix.
 
-        if name:
-            self._logger.name = f"cephci.{name}"
+    The primary reason we are doing this is to avoid duplicate log records when
+    multiple file handlers are attached to the same module. In case of parallel
+    test execution, there is a high probability that the same test module is
+    used to execute other test cases.
 
-        self._log_level = self._logger.getEffectiveLevel()
-        self._log_dir = None
-        self._log_errors = []
-        self.log_format = LOG_FORMAT
+    Creating a new log handler allows us to write logs to different files for
+    each test case using the same module and is being executed in parallel.
 
-    @property
-    def rp_logger(self):
-        return self.config.get("rp_logger")
+    Args:
+      name (str)    The name of the logger
 
-    @property
-    def logger(self) -> logging.Logger:
-        """Return the logger."""
-        return self._logger
+    Returns:
+        str The logger name.
+    """
+    rtn_value = name
+    if logging.getLogger(name).hasHandlers():
+        _prefix = "".join(random.choices(string.ascii_letters + string.digits, k=8))
+        rtn_value = f"{name}.{_prefix}"
 
-    @property
-    def log_dir(self) -> str:
-        """Return the absolute path to the logging folder."""
-        return self._log_dir
+    return rtn_value
 
-    @property
-    def log_level(self) -> int:
-        """Return the logging level."""
-        return self._log_level
 
-    @property
-    def config(self) -> Dict:
-        """Return the CephCI run configuration."""
-        return TestMetaData()
+def log_configure_defaults(console: bool = False) -> None:
+    """Configure logging defaults."""
+    logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 
-    @property
-    def run_id(self) -> str:
-        """Return the unique identifier of the execution run."""
-        return self.config.get("run_id")
+    if not console:
+        _logger = logging.getLogger()
+        _logger.propagate = False
 
-    @property
-    def metadata(self) -> Dict:
-        """Return the metadata of the execution run."""
-        return dict(
-            {
-                "test_run_id": self.run_id,
-                "testing_tool": "cephci",
-                "rhcs": self.config.get("rhcs"),
-                "test_build": self.config.get("rhbuild", "released"),
-                "rp_logger": self.config.get("rp_logger", None),
-            }
-        )
 
-    def _log(self, level: str, message: Any, *args, **kwargs) -> None:
-        """
-        Log the given message using the provided level along with the metadata.
-        updating LOG_FORMAT with filename:line_number - message
-        ex: 2022-11-15 11:37:00,346 - DEBUG - cephci.utility.log.py:227 - Completed log configuration
+def log_add_file_handler(logger_name, file_path, file_name, format=None) -> None:
+    """Adds a rotating file handler to the provided logger.
 
-        *Args:
-            level (str):        Log level
-            message (Any):      The message that needs to be logged
-        **kwargs:
-            metadata (dict):    Extra information to be appended to logstash
+    The purpose of this method is to add the a rotating file handler to the
+    given logger. There are two types of files created by this method. One for
+    information and other for errors.
 
-        Returns:
-            None.
-        """
-        log = {
-            "info": self._logger.info,
-            "debug": self._logger.debug,
-            "warning": self._logger.warning,
-            "error": self._logger.error,
-            "exception": self._logger.exception,
-        }
-        extra = deepcopy(self.metadata)
-        extra.update(kwargs.get("metadata", {}))
-        calling_frame = inspect.stack()[2].frame
-        trace = inspect.getframeinfo(calling_frame)
-        file_path = trace.filename.split("/")
-        files = file_path if len(file_path) == 1 else file_path[5:]
-        extra.update({"LINENUM": trace.lineno, "FILENAME": ".".join(files)})
-        log[level](
-            f"cephci.{extra['FILENAME']}:{extra['LINENUM']} - {message}",
-            *args,
-            extra=extra,
-            **kwargs,
-        )
+    Errors are logged separately so that they can be uploaded to ReportPortal.
 
-    def info(self, message: Any, *args, **kwargs) -> None:
-        """Log with info level the provided message and extra data.
+    Args:
+        logger_name (str)       The name of the logger to retrieve
+        file_path (Path)        The base directory to store the log files
+        file_name (str)         The prefix of the file name.
+        format (str)            Logging formatter. Defaults to LOG_FORMAT.
 
-        Args:
-            message (Any):  The message to be logged.
-            args (Any):     Dynamic list of supported arguments.
-            kwargs (Any):   Dynamic list of supported keyword arguments.
+    Returns
+        No output is returned.
+    """
+    _logger = logging.getLogger(logger_name)
+    if format:
+        _logger.setFormatter(format)
 
-        Returns:
-            None
-        """
-        self._log("info", message, *args, **kwargs)
+    # Check if there exists log file
+    _log_matches = glob.glob(os.path.join(file_path, f"{file_name}.*"))
+    if len(_log_matches) > 0:
+        _prefix = "".join(random.choices(string.ascii_letters + string.digits, k=8))
+        file_name = f"{file_name}-{_prefix}"
 
-    def debug(self, message: Any, *args, **kwargs) -> None:
-        """Log with debug level the provided message and extra data.
+    _log_file = os.path.join(file_path, f"{file_name}.log")
+    _err_file = os.path.join(file_path, f"{file_name}.err")
 
-        Args:
-            message (str):  The message to be logged.
-            args (Any):     Dynamic list of supported arguments.
-            kwargs (Any):   Dynamic list of supported keyword arguments.
-
-        Returns:
-            None
-        """
-
-        self._log("debug", message, *args, **kwargs)
-
-    def warning(self, message: Any, *args, **kwargs) -> None:
-        """Log with warning level the provided message and extra data.
-
-        Args:
-            message (Any):  The message to be logged.
-            args (Any):     Dynamic list of supported arguments.
-            kwargs (Any):   Dynamic list of supported keyword arguments.
-
-        Returns:
-            None
-        """
-        self._log("warning", message, *args, **kwargs)
-
-    def error(self, message: Any, *args, **kwargs) -> None:
-        """Log with error level the provided message and extra data.
-
-        Args:
-            message (Any):  The message to be logged.
-            args (Any):     Dynamic list of supported arguments.
-            kwargs (Any):   Dynamic list of supported keyword arguments.
-
-        Returns:
-            None
-        """
-        if self.rp_logger:
-            self.rp_logger.log(message=message, level="ERROR")
-
-        self._log("error", message, *args, **kwargs)
-        self._log_errors.append(message)
-
-    def exception(self, message: Any, *args, **kwargs) -> None:
-        """Log the given message under exception log level.
-
-        Args:
-            message (Any):  Message or record to be emitted.
-            args (Any):     Dynamic list of supported arguments.
-            kwargs (Any):   Dynamic list of supported keyword arguments.
-        Returns:
-            None
-        """
-        kwargs["exc_info"] = kwargs.get("exc_info", True)
-        self._log("exception", message, *args, **kwargs)
-
-    def configure_logger(self, test_name, run_dir, disable_console_log):
-        """
-        Configures a new FileHandler for the root logger.
-        Args:
-            test_name: name of the test being executed. used for naming the logfile
-            run_dir: directory where logs are being placed
-        Returns:
-            URL where the log file can be viewed or None if the run_dir does not exist
-        """
-        if not os.path.isdir(run_dir):
-            self._logger.error(
-                f"Run directory '{run_dir}' does not exist, logs will not output to file."
-            )
-            return None
-        self.close_and_remove_filehandlers()
-        log_format = logging.Formatter(self.log_format)
-        full_log_name = f"{test_name}.log"
-        test_logfile = os.path.join(run_dir, full_log_name)
-        self.info(f"Test logfile: {test_logfile}")
-        if disable_console_log:
-            self._logger.propagate = False
-        _handler = logging.FileHandler(test_logfile)
+    for _file in [_err_file, _log_file]:
         _handler = logging.handlers.RotatingFileHandler(
-            test_logfile,
-            maxBytes=10 * 1024 * 1024,  # Set the maximum log file size to 10 MB
-            backupCount=20,  # Keep up to 20 old log files which will be 200 MB per test case
+            _file,  # filename
+            maxBytes=10 * 1024 * 1024,  # 10MB max size
+            backupCount=20,  # keep up to 200MB
         )
-        _handler.setFormatter(log_format)
-        self._logger.addHandler(_handler)
-        # error file handler
-        err_logfile = os.path.join(run_dir, f"{test_name}.err")
-        _err_handler = logging.FileHandler(err_logfile)
-        _err_handler.setFormatter(log_format)
-        _err_handler.setLevel(logging.ERROR)
-        self._logger.addHandler(_err_handler)
+        _logger.addHandler(_handler)
 
-        url_base = (
-            magna_url + run_dir.split("/")[-1]
-            if "/ceph/cephci-jenkins" in run_dir
-            else run_dir
-        )
-        log_url = f"{url_base}/{full_log_name}"
-        self.debug("Completed log configuration")
 
-        return log_url
+def log_close_file_handlers(name) -> None:
+    """Close all file handlers."""
+    # remove handlers from root
+    logger = logging.getLogger(name)
 
-    def close_and_remove_filehandlers(self):
-        """
-        Close FileHandlers and then remove them from the loggers handlers list.
-        Returns:
-            None
-        """
-        handlers = self._logger.handlers[:]
-        for h in handlers:
-            if isinstance(h, logging.FileHandler):
-                h.close()
-                self._logger.removeHandler(h)
+    for _handler in logger.handlers:
+        if isinstance(_handler, logging.FileHandler):
+            _handler.close()
+            logger.removeHandler(_handler)
