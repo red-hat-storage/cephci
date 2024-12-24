228c107c370dd1e8d6ff071af61ae7aa8dca1be3
Srinivasa Bharath Kanta <skanta@redhat.com>
2024-12-23 20:03:34 -0500

    [TFA-Fix]- Reducing the execution time of scheduled scrub test suite
    Signed-off-by: Srinivasa Bharath Kanta <skanta@redhat.com>

--- tests/rados/scheduled_scrub_scenarios.py
+++ tests/rados/scheduled_scrub_scenarios.py
@@ -18,9 +18,9 @@ log = Log(__name__)
 
 
 def run(ceph_cluster, **kw):
-    osd_scrub_min_interval = 60
-    osd_scrub_max_interval = 3600
-    osd_deep_scrub_interval = 3600
+    osd_scrub_min_interval = 10
+    osd_scrub_max_interval = 300
+    osd_deep_scrub_interval = 300
 
     log.info(run.__doc__)
     config = kw["config"]
@@ -29,16 +29,22 @@ def run(ceph_cluster, **kw):
     mon_obj = MonConfigMethods(rados_obj=rados_obj)
 
     test_scenarios = ["scrub", "deep-scrub"]
+    # test_scenarios = ["deep-scrub"]
 
     for test_input in test_scenarios:
         # Creating replicated pool
         rp_config = config.get("replicated_pool")
         pool_name = rp_config["pool_name"]
+        log.info(f"===Start the  validation of the {test_input}=======")
+        if config.get("debug_enable"):
+            log.info("Set the OSD logs to 20")
+            mon_obj.set_config(section="osd", name="debug_osd", value="20/20")
 
         if not rados_obj.create_pool(name=pool_name, **rp_config):
             log.error("Failed to create the  Pool")
             return 1
-        rados_obj.bench_write(pool_name=pool_name, rados_write_duration=10)
+        rados_obj.bench_write(pool_name=pool_name, byte_size="5K", max_objs=200)
+        # rados_obj.bench_write(pool_name=, rados_write_duration=5)
         pg_id = rados_obj.get_pgid(pool_name=pool_name)
         log.info(f"The pg id is -{pg_id}")
         flag = wait_for_clean_pg_sets(rados_obj)
@@ -47,6 +53,12 @@ def run(ceph_cluster, **kw):
                 "The cluster did not reach active + Clean state after add capacity"
             )
             return 1
+        leader_mon_daemon = rados_obj.run_ceph_command(cmd="ceph mon stat")["leader"]
+        mon_host = rados_obj.fetch_host_node(
+            daemon_type="mon", daemon_id=leader_mon_daemon
+        )
+        init_time, _ = mon_host.exec_command(cmd="date '+%Y-%m-%d %H:%M:%S'", sudo=True)
+        log.info(f"The cluster time before  testing is - {init_time}")
         try:
             (
                 scrub_begin_hour,
@@ -85,10 +97,7 @@ def run(ceph_cluster, **kw):
 
             # set begin hour and end hour greater than current time
             # CEPH-9367 & CEPH 9368
-            if (
-                config.get("scenario") == "beginTime and endTime gt currentTime"
-                or config.get("scenario") == "decreaseTime"
-            ):
+            if config.get("scenario") == "beginTime and endTime gt currentTime":
                 (
                     scrub_begin_hour,
                     scrub_begin_weekday,
@@ -98,6 +107,10 @@ def run(ceph_cluster, **kw):
                 log.info(
                     f'{"Setting scrub start is greater then end_hour and current time is greater than end hour"}'
                 )
+                osd_scrub_min_interval = 86400
+                osd_scrub_max_interval = 604800
+                osd_deep_scrub_interval = 604800
+
             # unset the scrub.CEPH-9374
             if config.get("scenario") == "unsetScrub":
                 rados_obj.set_osd_flags("set", "noscrub")
@@ -141,7 +154,14 @@ def run(ceph_cluster, **kw):
                 )
                 rados_obj.set_osd_configuration("osd_scrub_end_hour", scrub_end_hour)
 
+            # Printing the scrub parameter values
+            get_parameter_configuration(mon_obj)
             status = chk_scrub_or_deepScrub_status(rados_obj, test_input, pg_id[-1])
+            end_time, _ = mon_host.exec_command(
+                cmd="date '+%Y-%m-%d %H:%M:%S'", sudo=True
+            )
+            log.info(f"The cluster time after testing is - {end_time}")
+
             # Checking the failure case
             if status is False and (
                 config.get("scenario") == "default"
@@ -159,6 +179,7 @@ def run(ceph_cluster, **kw):
             ) and status is True:
                 log.info(f"{test_input} validation Failed")
                 return 1
+            log.info(f"Deletion of the pool after the {test_input} validation")
             method_should_succeed(rados_obj.delete_pool, pool_name)
             time.sleep(10)
             flag = wait_for_clean_pg_sets(rados_obj)
@@ -167,6 +188,7 @@ def run(ceph_cluster, **kw):
                     "The cluster did not reach active + Clean state after add capacity"
                 )
                 return 1
+            log.info(f"===End of the  validation of the {test_input}=======")
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -179,11 +201,18 @@ def run(ceph_cluster, **kw):
             log.info(
                 "\n \n ************** Execution of finally block begins here *************** \n \n"
             )
+            mon_obj.remove_config(section="osd", name="debug_osd")
             method_should_succeed(rados_obj.delete_pool, pool_name)
             remove_parameter_configuration(mon_obj)
             rados_obj.set_osd_flags("unset", "noscrub")
             rados_obj.set_osd_flags("unset", "nodeep-scrub")
             time.sleep(10)
+            flag = wait_for_clean_pg_sets(rados_obj)
+            if not flag:
+                log.error(
+                    "The cluster did not reach active + Clean state after add capacity"
+                )
+                return 1
             # log cluster health
             rados_obj.log_cluster_health()
     log.error(f'{"Schedule scrub & deep-scrub validation success"}')
@@ -220,7 +249,7 @@ def chk_scrub_or_deepScrub_status(rados_object, action, pg_id):
     if action == "scrub":
         try:
             status = rados_object.start_check_scrub_complete(
-                pg_id=pg_id, user_initiated=False, wait_time=5400
+                pg_id=pg_id, user_initiated=False, wait_time=900
             )
             log.info("Verification of the scrub is completed")
         except Exception as err:
@@ -229,10 +258,34 @@ def chk_scrub_or_deepScrub_status(rados_object, action, pg_id):
     else:
         try:
             status = rados_object.start_check_deep_scrub_complete(
-                pg_id=pg_id, user_initiated=False, wait_time=5400
+                pg_id=pg_id, user_initiated=False, wait_time=900
             )
             log.info("Verification of the deep-scrub is completed")
         except Exception as err:
             log.error(f"The error is-{err}")
             status = False
     return status
+
+
+def get_parameter_configuration(mon_obj):
+    """
+    Used to get the  osd scrub parameter change values
+    Args:
+        mon_obj: monitor object
+    Returns : None
+    """
+    log.info("The scrub parameter values are:  ")
+    out_put = mon_obj.get_config(section="osd", param="osd_scrub_min_interval")
+    log.info(f"The osd_scrub_min_interval parameter value is - {out_put}")
+    out_put = mon_obj.get_config(section="osd", param="osd_scrub_max_interval")
+    log.info(f"The osd_scrub_max_interval parameter value is - {out_put}")
+    out_put = mon_obj.get_config(section="osd", param="osd_deep_scrub_interval")
+    log.info(f"The osd_deep_scrub_interval parameter value is - {out_put}")
+    out_put = mon_obj.get_config(section="osd", param="osd_scrub_begin_week_day")
+    log.info(f"The osd_scrub_begin_week_day parameter value is - {out_put}")
+    out_put = mon_obj.get_config(section="osd", param="osd_scrub_end_week_day")
+    log.info(f"The osd_scrub_end_week_day parameter value is - {out_put}")
+    out_put = mon_obj.get_config(section="osd", param="osd_scrub_begin_hour")
+    log.info(f"The osd_scrub_begin_hour parameter value is - {out_put}")
+    out_put = mon_obj.get_config(section="osd", param="osd_scrub_end_hour")
+    log.info(f"The osd_scrub_end_hour parameter value is - {out_put}")
