8ccee0fbcc7034fbce1de45e393a1ca4c19bf82b
Aarti Sharma <aarsharm@redhat.com>
2025-02-26 23:16:18 -0500

    Automation for CEPH-83584085: Live migration of raw data format
    Signed-off-by: Aarti Sharma <aarsharm@redhat.com>

--- ceph/rbd/workflows/migration.py
+++ ceph/rbd/workflows/migration.py
@@ -3,6 +3,8 @@ import tempfile
 
 from cli.rbd.rbd import Rbd
 from utility.log import Log
+from ceph.rbd.utils import random_string
+from ceph.rbd.workflows.rbd import create_single_pool_and_images
 
 log = Log(__name__)
 
@@ -73,3 +75,131 @@ def prepare_migration_source_spec(
     spec_file.flush()
 
     return temp_file.name
+
+def prepare_migration_source_spec(
+    cluster_name, client, pool_name, image_name, snap_name
+):
+    """
+    Create a native source spec file for migration.
+    Args:
+        cluster_name: Name of the source cluster
+        pool_name: Name of the source pool
+        image_name: Name of the source image
+        snap_name: Name of the snapshot
+    Returns:
+        Path to the native spec file
+    """
+    native_spec = {
+        "cluster_name": cluster_name,
+        "type": "native",
+        "pool_name": pool_name,
+        "image_name": image_name,
+        "snap_name": snap_name,
+    }
+
+    temp_file = tempfile.NamedTemporaryFile(dir="/tmp", suffix=".json")
+    spec_file = client.remote_file(sudo=True, file_name=temp_file.name, file_mode="w")
+    spec_file.write(json.dumps(native_spec, indent=4))
+    spec_file.flush()
+
+    return temp_file.name
+
+def run_prepare_execute_commit(rbd, pool, image, **kw):
+    """
+    Function to carry out the following:
+      - Create Target/destination pool for migration
+      - Migration prepare
+      - Migration Execute
+      - Migration commit
+    Args:
+        kw: rbd object, pool, image, test data
+    Returns:
+        int: The return value. 0 for success, 1 otherwise
+
+    """
+    # Create Target Pool/ Destination Pool for migration
+    is_ec_pool = True if "ec" in kw[pool]["pool_type"] else False
+    config = kw.get("config", {})
+    target_pool = "target_pool_" + random_string(len=3)
+    target_pool_config = {}
+    if is_ec_pool:
+        data_pool_target = "data_pool_new_" + random_string(len=3)
+        target_pool_config["data_pool"] = data_pool_target
+    rc = create_single_pool_and_images(
+        config=config,
+        pool=target_pool,
+        pool_config=target_pool_config,
+        client=kw["client"],
+        cluster="ceph",
+        rbd=rbd,
+        ceph_version=int(config.get("rhbuild")[0]),
+        is_ec_pool=is_ec_pool,
+        is_secondary=False,
+        do_not_create_image=True,
+    )
+    if rc:
+        log.error(f"Creation of target pool {target_pool} failed")
+        return rc
+
+    # Adding the new pool details to config so that they are handled in cleanup
+    if kw[pool]["pool_type"] == "rep_pool_config":
+        kw["config"]["rep_pool_config"][target_pool] = {}
+    elif kw[pool]["pool_type"] == "ec_pool_config":
+        kw["config"]["ec_pool_config"][target_pool] = {"data_pool": data_pool_target}
+
+    # Prepare Migration
+    target_image = "target_image_" + random_string(len=3)
+    rbd.migration.prepare(
+        source_spec=kw[pool]["spec"],
+        dest_spec=f"{target_pool}/{target_image}",
+        client_node=kw["client"],
+    )
+    kw[pool].update({"target_pool": target_pool})
+    kw[pool].update({"target_image": target_image})
+
+    # Verify prepare migration status
+    if verify_migration_state(
+        action="prepare",
+        image_spec=f"{target_pool}/{target_image}",
+        **kw,
+    ):
+        log.error("Failed to prepare migration")
+        return 1
+    else:
+        log.info("Migration prepare status verfied successfully")
+
+    # execute migration
+    rbd.migration.action(
+        action="execute",
+        dest_spec=f"{target_pool}/{target_image}",
+        client_node=kw["client"],
+    )
+
+    # verify execute migration status
+    if verify_migration_state(
+        action="execute",
+        image_spec=f"{target_pool}/{target_image}",
+        **kw,
+    ):
+        log.error("Failed to execute migration")
+        return 1
+    else:
+        log.info("Migration executed successfully")
+
+    # commit migration
+    rbd.migration.action(
+        action="commit",
+        dest_spec=f"{target_pool}/{target_image}",
+        client_node=kw["client"],
+    )
+
+    # verify commit migration status
+    if verify_migration_state(
+        action="commit",
+        image_spec=f"{target_pool}/{target_image}",
+        **kw,
+    ):
+        log.error("Failed to commit migration")
+        return 1
+    else:
+        log.info("Migration committed successfully")
