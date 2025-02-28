f386e4520acc7654db3122134f172149afab1762
SaiSuriya <surp@redhat.com>
2025-02-28 16:52:51 +0530

    added subcommands.py
    Signed-off-by: SaiSuriya <surp@redhat.com>

--- run.py
+++ run.py
@@ -34,6 +34,7 @@ from cli.performance.memory_and_cpu_utils import (
     upload_mem_and_cpu_logger_script,
 )
 from utility import sosreport
+from utility import subcommands
 from utility.log import Log
 from utility.polarion import post_to_polarion
 from utility.retry import retry
@@ -90,6 +91,7 @@ A simple test suite wrapper that executes tests based on yaml test configuration
         [--enable-eus]
         [--skip-enabling-rhel-rpms]
         [--skip-sos-report]
+        [--skip-subcommands]
         [--skip-tc <items>]
         [--monitor-performance]
         [--disable-console-log]
@@ -153,6 +155,7 @@ Options:
                                     rhel images for Interop runs
   --skip-sos-report                 Enables to collect sos-report on test suite failures
                                     [default: false]
+  --skip-subcommands                to capture the cli commands output                  
   --skip-tc <items>                 skip test case provided in comma seperated fashion
   --monitor-performance             Monitor performance and CPU usage on all/required nodes
                                     for every test and collects data to specified dir
@@ -486,6 +489,7 @@ def run(args):
     enable_eus = args.get("--enable-eus")
     skip_enabling_rhel_rpms = args.get("--skip-enabling-rhel-rpms")
     skip_sos_report = args.get("--skip-sos-report")
+    skip_subcommands = args.get("--skip-subcommands")
 
     # load config, suite and inventory yaml files
     conf = load_file(glb_file)
@@ -986,6 +990,8 @@ def run(args):
         "invoked-by": trigger_user,
     }
 
+
+
     if xunit_results:
         create_xunit_results(suite_name, tcs, test_run_metadata)
 
@@ -1029,6 +1035,9 @@ def run(args):
             # This can be Removed as sos report will have this details as well
             get_ceph_var_logs(ceph_cluster_dict[cluster], run_dir)
         log.info(f"Generated sosreports location : {url_base}/sosreports\n")
+
+    if not skip_subcommands:
+        subcommands.run(run_dir, "rgw",url_base)
 
     return jenkins_rc
 
