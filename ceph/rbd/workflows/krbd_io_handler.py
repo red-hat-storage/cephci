7b35bf7255b5df5bfba00f469da914c21f31e535
Aarti Sharma <aarsharm@redhat.com>
2025-04-15 13:32:22 -0400

    Modified default values for FIO params
    Signed-off-by: Aarti Sharma <aarsharm@redhat.com>

--- ceph/rbd/workflows/krbd_io_handler.py
+++ ceph/rbd/workflows/krbd_io_handler.py
@@ -155,11 +155,11 @@ def krbd_io_handler(**kw):
                         get_time_taken=config.get("get_time_taken", False),
                         cmd_timeout=config.get("cmd_timeout"),
                         io_type=config.get("io_type", "write"),
-                        num_jobs=config.get("num_jobs", "1"),
-                        iodepth=config.get("iodepth", "8"),
-                        rwmixread=config.get("rwmixread", "50"),
-                        direct=config.get("direct", "0"),
-                        invalidate=config.get("invalidate", "0"),
+                        num_jobs=config.get("num_jobs", "4"),
+                        iodepth=config.get("iodepth", "32"),
+                        rwmixread=config.get("rwmixread", "70"),
+                        direct=config.get("direct", "1"),
+                        invalidate=config.get("invalidate", "1"),
                     )
                 else:
                     fio_args = {
