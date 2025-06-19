373d9fb28ec081cf28012ff8d8134248752522ea
Harishacharya-redhat <hacharya@redhat.com>
2025-06-19 16:23:22 +0530

    adding longivity tests
    Signed-off-by: Harishacharya-redhat <hacharya@redhat.com>

--- tests/nfs/test_nfs_io_operations_during_upgrade.py
+++ tests/nfs/test_nfs_io_operations_during_upgrade.py
@@ -1,16 +1,16 @@
+import time
 from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 
 from ceph.waiter import WaitUntil
 from cli.ceph.ceph import Ceph
 from cli.exceptions import ConfigError, OperationFailedError
-from cli.utilities.filesys import Mount, Unmount
+from cli.utilities.filesys import Unmount
 from tests.nfs.nfs_operations import (
     NfsCleanupFailed,
-    _get_client_specific_mount_versions,
-    exports_mounts_perclient,
+    create_export_and_mount_for_existing_nfs_cluster,
 )
-from tests.nfs.test_nfs_multiple_operations_for_upgrade import (
+from tests.nfs.nfs_utils import (
     create_file,
     delete_file,
     read_from_file_using_dd_command,
@@ -18,92 +18,11 @@ from tests.nfs.test_nfs_multiple_operations_for_upgrade import (
     write_to_file_using_dd_command,
 )
 from utility.log import Log
-from utility.retry import retry
 
 log = Log(__name__)
 
 
-@retry(OperationFailedError, tries=6, delay=10, backoff=4)
-def mount_retry(
-    clients, client_num, mount_name, version, port, nfs_server, export_name
-):
-    if Mount(clients[client_num]).nfs(
-        mount=mount_name,
-        version=version,
-        port=port,
-        server=nfs_server,
-        export=export_name,
-    ):
-        return True
-    else:
-        raise OperationFailedError(
-            "Failed to mount nfs on %s" % clients[client_num].hostname
-        )
-
-
-def create_export_and_mount_for_existing_nfs_cluster(
-    clients,
-    nfs_export,
-    nfs_mount,
-    export_num,
-    fs_name,
-    nfs_name,
-    fs,
-    port,
-    version="4.0",
-    ha=False,
-    vip=None,
-    nfs_server=None,
-):
-
-    client_export_mount_dict = exports_mounts_perclient(
-        clients, nfs_export, nfs_mount, export_num
-    )
-    for client_num in range(len(clients)):
-        for export_num in range(
-            len(client_export_mount_dict[clients[client_num]]["export"])
-        ):
-            export_name = client_export_mount_dict[clients[client_num]]["export"][
-                export_num
-            ]
-            mount_name = client_export_mount_dict[clients[client_num]]["mount"][
-                export_num
-            ]
-            Ceph(clients[client_num]).nfs.export.create(
-                fs_name=fs_name,
-                nfs_name=nfs_name,
-                nfs_export=export_name,
-                fs=fs,
-            )
-            sleep(1)
-            # Get the mount versions specific to clients
-            mount_versions = _get_client_specific_mount_versions(version, clients)
-            # Step 4: Perform nfs mount
-            # If there are multiple nfs servers provided, only one is required for mounting
-            if isinstance(nfs_server, list):
-                nfs_server = nfs_server[0]
-            if ha:
-                nfs_server = vip.split("/")[0]  # Remove the port
-            for version, clients in mount_versions.items():
-                clients[client_num].create_dirs(dir_path=mount_name, sudo=True)
-
-                if Mount(clients[client_num]).nfs(
-                    mount=mount_name,
-                    version=version,
-                    port=port,
-                    server=nfs_server,
-                    export=export_name,
-                ):
-                    raise OperationFailedError(
-                        "Failed to mount nfs on %s" % clients[client_num].hostname
-                    )
-                sleep(1)
-        log.info("Mount succeeded on all clients")
-
-    return client_export_mount_dict
-
-
-def perform_io_operations_in_loop(
+def perform_io_operations(
     client_export_mount_dict, clients, file_count, dd_command_size_in_M
 ):
     """
@@ -262,28 +181,33 @@ def remove_exports_and_unmount(client_export_mount_dict, clients, nfs_name):
 
 def run(ceph_cluster, **kw):
     """
-    1, pick the existing NFS cluster
+    1. pick the existing NFS cluster
     2. create multiple exports
     3. mount to clients
     4. create a folder so all the oparation from this case will run in that folder
     5. perform Create,read,rename,copy, read/write using DD command and deletion in a loop may be 10 or 20 times
     6. unmount + delete exports
-    7.reapeat 1,2,3,4,5, and 6th steps in a loop - 10 or 20 times
-    8.donnot delete the cluster
+    7. repeat 1,2,3,4,5, and 6th steps in a loop - 10 or 20 times or for a given time in hours
+    8. do not delete the cluster
     """
     config = kw.get("config")
     clients = ceph_cluster.get_nodes("client")
 
     port = config.get("port", "2049")
     loop_count = config.get("loop_count", None)
+    longevity = config.get("longevity", False)
+    longevity_duration = float(config.get("longevity_duration", 0))
+    longevity_loop = config.get("longevity_loop", None)
+    longevity = config.get("longevity", False)  # in hours
     version = config.get("nfs_version", "4.0")
     no_clients = int(config.get("clients", "2"))
     file_count = int(config.get("file_count", "100"))
     dd_command_size_in_M = config.get("dd_command_size_in_M", "100")
     export_num = config.get("exports_number", 1)
-    ha = bool(config.get("ha", False))
+    nfs_cluster_name = config.get("cluster_name", None)
+    ha = config.get("ha", False)
     vip = config.get("vip", None)
-
+    loop_count = longevity_loop if longevity else loop_count
     # If the setup doesn't have required number of clients, exit.
     if no_clients > len(clients):
         raise ConfigError("The test requires more clients than available")
@@ -292,48 +216,111 @@ def run(ceph_cluster, **kw):
     client = clients[0]  # Select only the required number of clients
 
     # 1. pick the existing NFS cluster
-    nfs_cluster_name = Ceph(client).nfs.cluster.ls()[0]
+    if nfs_cluster_name is None:
+        nfs_cluster_name = Ceph(client).nfs.cluster.ls()[0]
     nfs_hostname = Ceph(client).nfs.cluster.info(nfs_cluster_name)[nfs_cluster_name][
         "backend"
     ][0]["hostname"]
     try:
-        for l in range(loop_count):
+        if longevity and longevity_duration > 0:
+            start_time = time.time()
+            duration_seconds = longevity_duration * 3600
+            loop = 0
             log.info(
-                f"Starting NFS IO operations loop \n "
-                f"Loop - {l + 1}/{loop_count} \n" + "=" * 30 + "\n"
+                "\n \n"
+                + "->" * 30
+                + f" Running longevity for {longevity_duration} hours "
+                + "<-" * 30
+                + "\n"
             )
+            while (time.time() - start_time) < duration_seconds:
+                loop += 1
+                log.info(
+                    "\n \n"
+                    + "=" * 30
+                    + f"\n Running longevity loop {loop} (time-based) \n "
+                    + "=" * 30
+                )
+                # 2. create multiple exports
+                # 3. mount to clients
+                client_export_mount_dict = (
+                    create_export_and_mount_for_existing_nfs_cluster(
+                        clients,
+                        f"/export_nfs_{nfs_cluster_name}",
+                        f"/mnt/nfs_{nfs_cluster_name}",
+                        export_num,
+                        fs_name="cephfs",
+                        nfs_name=nfs_cluster_name,
+                        fs="cephfs",
+                        port=port,
+                        version=version,
+                        ha=ha,
+                        nfs_server=nfs_hostname,
+                        vip=vip,
+                    )
+                )
 
-            # 2. create multiple exports
-            # 3. mount to clients
-            client_export_mount_dict = create_export_and_mount_for_existing_nfs_cluster(
-                clients,
-                f"/export/nfs_{nfs_cluster_name}",
-                f"/mnt/nfs_{nfs_cluster_name}",
-                export_num,
-                fs_name="cephfs",
-                nfs_name=nfs_cluster_name,
-                fs="cephfs",
-                port=port,
-                version=version,
-                ha=ha,
-                nfs_server=nfs_hostname,
-                vip=vip,
-            )
+                # 4. perform Create, read, rename, copy, read/write using DD command and deletion
+                perform_io_operations(
+                    client_export_mount_dict,
+                    clients,
+                    file_count,
+                    dd_command_size_in_M,
+                )
 
-            # 4. perform Create, read, rename, copy, read/write using DD command and deletion
-            perform_io_operations_in_loop(
-                client_export_mount_dict,
-                clients,
-                file_count,
-                dd_command_size_in_M,
-            )
+                # 5. unmount + delete exports
+                log.info("Unmounting and deleting exports")
+                remove_exports_and_unmount(
+                    client_export_mount_dict, clients, nfs_cluster_name
+                )
 
-            # 5. unmount + delete exports
-            log.info("Unmounting and deleting exports")
-            remove_exports_and_unmount(
-                client_export_mount_dict, clients, nfs_cluster_name
-            )
-        return 0
+                log.info(
+                    "NFS IO operations loop completed successfully. -- "
+                    "remaining time: {:.2f} minutes".format(
+                        (duration_seconds - (time.time() - start_time)) / 60
+                    )
+                )
+            log.info("NFS IO operations completed successfully.")
+            return 0
+        else:
+            for l in range(loop_count):
+                log.info(
+                    f"Starting NFS IO operations loop\n"
+                    f"Loop - {l + 1}/{loop_count}\n" + "=" * 30 + "\n"
+                )
+                # 2. create multiple exports
+                # 3. mount to clients
+                client_export_mount_dict = (
+                    create_export_and_mount_for_existing_nfs_cluster(
+                        clients,
+                        f"/export_nfs_{nfs_cluster_name}",
+                        f"/mnt/nfs_{nfs_cluster_name}",
+                        export_num,
+                        fs_name="cephfs",
+                        nfs_name=nfs_cluster_name,
+                        fs="cephfs",
+                        port=port,
+                        version=version,
+                        ha=ha,
+                        nfs_server=nfs_hostname,
+                        vip=vip,
+                    )
+                )
+
+                # 4. perform Create, read, rename, copy, read/write using DD command and deletion
+                perform_io_operations(
+                    client_export_mount_dict,
+                    clients,
+                    file_count,
+                    dd_command_size_in_M,
+                )
+
+                # 5. unmount + delete exports
+                log.info("Unmounting and deleting exports")
+                remove_exports_and_unmount(
+                    client_export_mount_dict, clients, nfs_cluster_name
+                )
+            return 0
     except Exception as e:
         log.error(f"An error occurred during NFS IO operations: {e}")
         raise OperationFailedError(f"NFS IO operations failed: {e}")
