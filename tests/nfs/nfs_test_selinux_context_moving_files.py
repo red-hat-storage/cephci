38e2187f228bbe1dfbab1a3b8a99a3c55925a72f
SinghSamosa <manishsingh_07@yahoo.in>
2025-08-19 16:28:04 +0530

    tfa_fix_nfs_test_setting_selinux_context.py_&_nfs_test_selinux_context_moving_files.py
    Signed-off-by: SinghSamosa <manishsingh_07@yahoo.in>

--- tests/nfs/nfs_test_selinux_context_moving_files.py
+++ tests/nfs/nfs_test_selinux_context_moving_files.py
@@ -48,13 +48,6 @@ def run(ceph_cluster, **kw):
             ceph_cluster=ceph_cluster,
         )
 
-        # Mount the share on Client 2
-        cmd = f"umount -l {nfs_mount}"
-        clients[1].exec_command(sudo=True, cmd=cmd)
-
-        cmd = f"mount -t nfs {nfs_nodes[0].ip_address}:{nfs_export}_0 {nfs_mount}"
-        clients[1].exec_command(sudo=True, cmd=cmd)
-
         # Create 5 file on Mount point from client 1
         try:
             for i in range(1, num_files + 1):
