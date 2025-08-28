598ccb9375eaefa12d1a27092196ac9fd364fe5c
Harsh Kumar <hakumar@redhat.com>
2025-08-28 06:17:59 +0530

    [RADOS] RHCEPHQE-21199: Fix logging and remove invocation of invalid method in test_rados_watcher_cleanup.py
    Signed-off-by: Harsh Kumar <hakumar@redhat.com>

--- ceph/rados/core_workflows.py
+++ ceph/rados/core_workflows.py
@@ -4432,7 +4432,7 @@ EOF"""
 
         if mnt_name in out:
             log.info("Verification successful. Device is listed in mount points.")
-            return f"{mnt_path}{mnt_name}"
+            return mnt_path, mnt_name
         else:
             log.error("Verification failed. Device is not listed in mount points.")
             return None
